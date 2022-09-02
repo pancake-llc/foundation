1868e039651560d4f651c979a505254fa2de0119
yenmoc <phongsoyenmoc.diep@gmail.com>
2022-09-02 15:34:25 +0700

    *update info

--- Assets/_Root/Editor/Wizard/Wizard.cs
+++ Assets/_Root/Editor/Wizard/Wizard.cs
@@ -42,8 +42,8 @@ namespace Pancake.Editor
             {
                 case EWizardPage.Debug:
                     GUILayout.Label("Installation content", Styles.title);
-                    GUILayout.Label("<color=#93DD59>1)</color> Dll DebugBuild and Dll ReleaseBuild", Styles.description);
-                    GUILayout.Label("<color=#93DD59>2)</color> ScriptableObject for debug extension project setting", Styles.description);
+                    GUILayout.Label("<color=#93DD59>1)</color> Debug dll for debug build and release build", Styles.description);
+                    GUILayout.Label("<color=#93DD59>2)</color> ScriptableObject debug extension project setting in Resources folder", Styles.description);
                     GUILayout.Label("<color=#93DD59>3)</color> Script Chanel.cs", Styles.description);
                     break;
                 default:
