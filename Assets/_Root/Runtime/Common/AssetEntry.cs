48d8e2ca8551449ab1696b7cc770aa64e8415c8e
yenmoc <phongsoyenmoc.diep@gmail.com>
2022-09-10 21:31:51 +0700

    *add asset entry

---
+++ Assets/_Root/Runtime/Common/AssetEntry.cs
@@ -0,0 +1,15 @@
+namespace Pancake
+{
+    [System.Serializable]
+    internal sealed class AssetEntry
+    {
+        [field: UnityEngine.SerializeField] public string Guid { get; private set; }
+        [field: UnityEngine.SerializeField] public UnityEngine.Object Asset { get; private set; }
+
+        public AssetEntry(string guid, UnityEngine.Object asset)
+        {
+            Guid = guid;
+            Asset = asset;
+        }
+    }
+}
