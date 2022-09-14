73bc376a145377af3d27eb5cc0c30a8dbc820e63
yenmoc <phongsoyenmoc.diep@gmail.com>
2022-09-15 05:47:27 +0700

    *update asset formatter

--- Assets/_Root/Runtime/Archive/AssetFormatter.cs
+++ Assets/_Root/Runtime/Archive/AssetFormatter.cs
@@ -5,8 +5,16 @@ namespace Pancake
 {
     public class AssetFormatter<T> : IMessagePackFormatter<T> where T : UnityEngine.Object
     {
-        public void Serialize(ref MessagePackWriter writer, T value, MessagePackSerializerOptions options) { throw new System.NotImplementedException(); }
+        public void Serialize(ref MessagePackWriter writer, T value, MessagePackSerializerOptions options)
+        {
+            Archive.Container.TryResolveId(value, out string id);
+            writer.Write(id);
+        }
 
-        public T Deserialize(ref MessagePackReader reader, MessagePackSerializerOptions options) { throw new System.NotImplementedException(); }
+        public T Deserialize(ref MessagePackReader reader, MessagePackSerializerOptions options)
+        {
+            Archive.Container.TryResolveReference(reader.ReadString(), out object value);
+            return (T)value;
+        }
     }
 }
