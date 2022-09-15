4a1b0f6cb0e606b0f40f54ef352bfe795cbe5c75
yenmoc <phongsoyenmoc.diep@gmail.com>
2022-09-15 07:34:10 +0700

    *update tools menu

--- Assets/_Root/Editor/Main/MenuToolsCreator.cs
+++ Assets/_Root/Editor/Main/MenuToolsCreator.cs
@@ -1,11 +1,12 @@
 using JetBrains.Annotations;
 using UnityEditor;
+using UnityEngine;
 
 namespace Pancake.Editor
 {
     public static class MenuToolsCreator
     {
-        [MenuItem("Tools/Project Settings", priority = 100000), UsedImplicitly]
+        [MenuItem("Tools/Project Settings", priority = 30000), UsedImplicitly]
         private static void OpenSettings()
         {
 #if UNITY_2019_1_OR_NEWER
@@ -15,7 +16,7 @@ namespace Pancake.Editor
 #endif
         }
 
-        [MenuItem("Tools/User Preferences", priority = 100000), UsedImplicitly]
+        [MenuItem("Tools/User Preferences", priority = 30000), UsedImplicitly]
         private static void OpenPreferences()
         {
 #if UNITY_2019_1_OR_NEWER
@@ -23,6 +24,33 @@ namespace Pancake.Editor
 #else
 			EditorApplication.ExecuteMenuItem("Edit/Project Settings...");
 #endif
+        }
+        
+        [MenuItem("Tools/Open Persistent Data Path", false, 31000), UsedImplicitly]
+        private static void OpenPersistentDataPath() { EditorUtility.RevealInFinder(Application.persistentDataPath); }
+
+        [MenuItem("Tools/Clear Persistent Data Path", false, 31000), UsedImplicitly]
+        private static void ClearPersistentDataPath()
+        {
+            if (EditorUtility.DisplayDialog("Clear Persistent Data Path",
+                    "Are you sure you wish to clear the persistent data path?\n This action cannot be reversed.",
+                    "Clear",
+                    "Cancel"))
+            {
+                System.IO.DirectoryInfo di = new System.IO.DirectoryInfo(Application.persistentDataPath);
+
+                foreach (System.IO.FileInfo file in di.GetFiles())
+                    file.Delete();
+                foreach (System.IO.DirectoryInfo dir in di.GetDirectories())
+                    dir.Delete(true);
+            }
+        }
+
+        [MenuItem("Tools/Clear PlayerPrefs", false, 31000), UsedImplicitly]
+        private static void ClearPlayerPrefs()
+        {
+            if (EditorUtility.DisplayDialog("Clear PlayerPrefs", "Are you sure you wish to clear PlayerPrefs?\nThis action cannot be reversed.", "Clear", "Cancel"))
+                PlayerPrefs.DeleteAll();
         }
     }
 }
