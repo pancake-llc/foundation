26c4d58ca4561cdb258f1d5dc336348a2e68853b
yenmoc <phongsoyenmoc.diep@gmail.com>
2022-09-30 16:15:56 +0700

    *add linq chunk

--- Assets/_Root/Runtime/Common/Error.cs
+++ Assets/_Root/Runtime/Common/Error.cs
@@ -8,80 +8,54 @@ namespace Pancake
     internal static partial class Error
     {
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static void ThrowArgumentNullException<T>(T value, string paramName)
-          where T : class
+        public static void ThrowArgumentNullException<T>(T value, string paramName) where T : class
         {
             if (value == null) ThrowArgumentNullExceptionCore(paramName);
         }
 
         [MethodImpl(MethodImplOptions.NoInlining)]
-        static void ThrowArgumentNullExceptionCore(string paramName)
-        {
-            throw new ArgumentNullException(paramName);
-        }
+        static void ThrowArgumentNullExceptionCore(string paramName) { throw new ArgumentNullException(paramName); }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static Exception ArgumentOutOfRange(string paramName)
-        {
-            return new ArgumentOutOfRangeException(paramName);
-        }
+        public static Exception ArgumentOutOfRange(string paramName) { return new ArgumentOutOfRangeException(paramName); }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static Exception NoElements()
-        {
-            return new InvalidOperationException("Source sequence doesn't contain any elements.");
-        }
+        public static Exception NoElements() { return new InvalidOperationException("Source sequence doesn't contain any elements."); }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static Exception MoreThanOneElement()
-        {
-            return new InvalidOperationException("Source sequence contains more than one element.");
-        }
+        public static Exception MoreThanOneElement() { return new InvalidOperationException("Source sequence contains more than one element."); }
 
         [MethodImpl(MethodImplOptions.NoInlining)]
-        public static void ThrowArgumentException<T>(string message)
-        {
-            throw new ArgumentException(message);
-        }
+        public static void ThrowArgumentException<T>(string message) { throw new ArgumentException(message); }
 
         [MethodImpl(MethodImplOptions.NoInlining)]
-        public static void ThrowNotYetCompleted()
-        {
-            throw new InvalidOperationException("Not yet completed.");
-        }
+        public static void ThrowNotYetCompleted() { throw new InvalidOperationException("Not yet completed."); }
 
         [MethodImpl(MethodImplOptions.NoInlining)]
-        public static T ThrowNotYetCompleted<T>()
-        {
-            throw new InvalidOperationException("Not yet completed.");
-        }
+        public static T ThrowNotYetCompleted<T>() { throw new InvalidOperationException("Not yet completed."); }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static void ThrowWhenContinuationIsAlreadyRegistered<T>(T continuationField)
-          where T : class
+        public static void ThrowWhenContinuationIsAlreadyRegistered<T>(T continuationField) where T : class
         {
             if (continuationField != null) ThrowInvalidOperationExceptionCore("continuation is already registered.");
         }
 
         [MethodImpl(MethodImplOptions.NoInlining)]
-        static void ThrowInvalidOperationExceptionCore(string message)
-        {
-            throw new InvalidOperationException(message);
-        }
+        static void ThrowInvalidOperationExceptionCore(string message) { throw new InvalidOperationException(message); }
 
         [MethodImpl(MethodImplOptions.NoInlining)]
-        public static void ThrowOperationCanceledException()
-        {
-            throw new OperationCanceledException();
-        }
-        
+        public static void ThrowOperationCanceledException() { throw new OperationCanceledException(); }
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
         internal static Exception ArgumentNull(string s) { return new ArgumentNullException(s); }
 
+        [MethodImpl(MethodImplOptions.NoInlining)]
         internal static Exception MoreThanOneMatch() { return new InvalidOperationException("Sequence contains more than one matching element"); }
 
+        [MethodImpl(MethodImplOptions.NoInlining)]
         internal static Exception NoMatch() { return new InvalidOperationException("Sequence contains no matching element"); }
-    
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
         internal static Exception NotSupported() { return new NotSupportedException(); }
     }
 }
-
