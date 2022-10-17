13f1500db7a0e3c7e383e407e70ea0fb1c58789e
yenmoc <phongsoyenmoc.diep@gmail.com>
2022-10-18 00:28:12 +0700

    *refactor

--- Assets/_Root/Runtime/Core/Enum.cs
+++ Assets/_Root/Runtime/Core/Enum.cs
@@ -102,4 +102,16 @@ namespace Pancake
         Normal = 0,
         Unscaled = 1,
     }
+    
+    /// <summary>
+    /// UpdateMode
+    /// </summary>
+    public enum UpdateMode
+    {
+        Update = 0,
+        LateUpdate = 1,
+        FixedUpdate = 2,
+        WaitForFixedUpdate = 3,
+        WaitForEndOfFrame = 4
+    }
 }
