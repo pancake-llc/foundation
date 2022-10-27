7479cd0e2d338a3eeb50b8ed92a11148abc62505
yenmoc <phongsoyenmoc.diep@gmail.com>
2022-10-27 18:36:19 +0700

    *add singleton setting

---
+++ Assets/_Root/Runtime/Common/SingletonSettings.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+namespace Pancake
+{
+    public abstract class SingletonSettings<T> : ScriptableObject where T : ScriptableObject
+    {
+        private static T instance;
+
+        public static T Instance
+        {
+            get
+            {
+                if (instance != null) return instance;
+
+                instance = Resources.Load<T>(nameof(T));
+
+                if (instance == null)
+                {
+#if UNITY_EDITOR
+                    Debug.LogWarning($"{nameof(T)} not found! Please check again!");
+#endif
+                    instance = CreateInstance<T>();
+                }
+                
+                return instance;
+            }
+        }
+        
+        public static T Load() => Resources.Load<T>(nameof(T));
+    }
+}
