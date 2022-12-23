89cb56d53e2897b4eadc63f6538d9064484a2c92
yenmoc <phongsoyenmoc.diep@gmail.com>
2022-12-23 15:30:22 +0700

    *refactor

--- Runtime/IAP/IAPSettings.cs
+++ Runtime/IAP/IAPSettings.cs
@@ -50,7 +50,7 @@ namespace Pancake.IAP
                 str += "\n\t\t}";
                 str += "\n";
                 
-                str += $"\n\t\tpublic static bool IsPurchase{System.Globalization.CultureInfo.CurrentCulture.TextInfo.ToTitleCase(itemName)}()";
+                str += $"\n\t\tpublic static bool IsPurchased{System.Globalization.CultureInfo.CurrentCulture.TextInfo.ToTitleCase(itemName)}()";
                 str += "\n\t\t{";
                 str += $"\n\t\t\treturn IAPManager.Instance.IsPurchased(IAPSettings.SkusData[{i}].sku.Id);";
                 str += "\n\t\t}";
