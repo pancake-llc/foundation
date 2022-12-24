92d7d4a06df11766111a5d170a83677d087939c3
yenmoc <phongsoyenmoc.diep@gmail.com>
2022-12-24 20:39:13 +0700

    *normalize

--- Runtime/Core/Mono/MonoAllocation.cs
+++ Runtime/Core/Mono/MonoAllocation.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Runtime.CompilerServices;
 using UnityEngine;
 using Object = UnityEngine.Object;
 
@@ -7,103 +6,28 @@ namespace Pancake
 {
     public class MonoAllocation : MonoBehaviour
     {
-        private Dictionary<int, Component> _get;
-        private Dictionary<int, Component[]> _gets;
-
-        private Dictionary<int, Component> _childrenGet;
-        private Dictionary<int, Component[]> _childrenGets;
-
-        private Dictionary<int, Component> _parentGet;
-        private Dictionary<int, Component[]> _parentGets;
-
-        private Dictionary<int, Component> _find;
-        private Dictionary<int, Component[]> _finds;
-
-        private bool _allocationEnabled = true;
-
-        public void EnableAllocation() { _allocationEnabled = true; }
-
-        public void DisableAllocation() { _allocationEnabled = false; }
-
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public T Get<T>() => GetComponent<T>();
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public T[] Gets<T>() => GetComponents<T>();
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public T ChildrenGet<T>() => GetComponentInChildren<T>();
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public T[] ChildrenGets<T>() => GetComponentsInChildren<T>();
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public T ParentGet<T>() => GetComponentInParent<T>();
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public T[] ParentGets<T>() => GetComponentsInParent<T>();
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public T Find<T>() where T : Object => FindObjectOfType<T>();
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public T[] Finds<T>() where T : Object => FindObjectsOfType<T>();
-
-
-        public T GetCached<T>() where T : Component { return GetSingleCached(ref _get, GetComponent<T>); }
-
-        public T[] GetsCached<T>() where T : Component { return GetManyCached(ref _gets, GetComponents<T>); }
-
-        public T ChildrenGetCached<T>() where T : Component { return GetSingleCached(ref _childrenGet, GetComponentInChildren<T>); }
-
-        public T[] ChildrenGetsCached<T>() where T : Component { return GetManyCached(ref _childrenGets, GetComponentsInChildren<T>); }
-
-        public T ParentGetCached<T>() where T : Component { return GetSingleCached(ref _parentGet, GetComponentInParent<T>); }
-
-        public T[] ParentGetsCached<T>() where T : Component { return GetManyCached(ref _parentGets, GetComponentsInParent<T>); }
-
-        public T FindCached<T>() where T : Component { return GetSingleCached(ref _find, FindObjectOfType<T>); }
-
-        public T[] FindsCached<T>() where T : Component { return GetManyCached(ref _finds, FindObjectsOfType<T>); }
-
-        private T GetSingleCached<T>(ref Dictionary<int, Component> storage, Func<T> getMethod) where T : Component
-        {
-            var index = GetInfo<T>.Index;
-
-            if (_allocationEnabled)
-            {
-                storage ??= new Dictionary<int, Component>(16);
-
-                if (storage.TryGetValue(index, out var component))
-                {
-                    return (T) component;
-                }
-            }
-
-            var instance = getMethod?.Invoke();
-
-            if (_allocationEnabled && instance != null)
-            {
-                storage.Add(index, instance);
-            }
-
-            return instance;
-        }
-
-        private T[] GetManyCached<T>(ref Dictionary<int, Component[]> storage, Func<T[]> getsMethod) where T : Component
-        {
-            var index = GetInfo<T>.Index;
-
-            if (_allocationEnabled)
-            {
-                storage ??= new Dictionary<int, Component[]>(16);
-
-                if (storage.TryGetValue(index, out var components))
-                {
-                    return (T[]) components;
-                }
-            }
-
-            var instances = getsMethod?.Invoke();
-
-            if (_allocationEnabled && instances != null)
-            {
-                storage.Add(index, instances);
-            }
-
-            return instances;
-        }
     }
 }
