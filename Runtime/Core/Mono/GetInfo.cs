e1124bb76822534a6b967d5023aaad4866d75cc6
yenmoc <phongsoyenmoc.diep@gmail.com>
2022-12-24 13:48:13 +0700

    *add mono allocation

---
+++ Runtime/Core/Mono/GetInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading;
+
+namespace Pancake
+{
+    public static class GetInfo<T>
+    {
+        public static Type Type { get; }
+        public static int Index { get; }
+
+        static GetInfo()
+        {
+            Type = typeof(T);
+            Index = Interlocked.Increment(ref IndexIncrement.index);
+        }
+    }
+}
---
+++ Runtime/Core/Mono/IndexIncrement.cs
@@ -0,0 +1,7 @@
+﻿namespace Pancake
+{
+    internal static class IndexIncrement
+    {
+        public static int index;
+    }
+}
