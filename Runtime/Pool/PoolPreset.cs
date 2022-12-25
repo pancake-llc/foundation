95cd28f11828720266db4dea8b191059fea4dc32
yenmoc <phongsoyenmoc.diep@gmail.com>
2022-12-25 20:39:17 +0700

    *add description in pool preset

--- Runtime/Pool/PoolPreset.cs
+++ Runtime/Pool/PoolPreset.cs
@@ -6,6 +6,7 @@ namespace Pancake
     [CreateAssetMenu(fileName = "PoolPreset", menuName = "Pancake/Create Pool Preset", order = 0)]
     public class PoolPreset : ScriptableObject
     {
+        [SerializeField, TextArea] private string developerDescription;
         [SerializeField] private string poolName;
         [SerializeField] private List<PoolObject> poolObjects = new List<PoolObject>(256);
 
