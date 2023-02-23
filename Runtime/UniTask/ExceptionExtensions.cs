45027947661af106a63fde800f6bf43b92189e3a
yenmoc <phongsoyenmoc.diep@gmail.com>
2023-02-24 00:25:45 +0700

    -add: UniTask

---
+++ Runtime/UniTask/ExceptionExtensions.cs
@@ -0,0 +1,14 @@
+﻿
+using System;
+
+namespace Pancake.Threading.Tasks
+{
+    public static class ExceptionExtensions
+    {
+        public static bool IsOperationCanceledException(this Exception exception)
+        {
+            return exception is OperationCanceledException;
+        }
+    }
+}
+
