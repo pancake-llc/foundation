fe9e6b07f1cdef77ccaf3bb8ff4ff1ffdc1690e8
yenmoc <phongsoyenmoc.diep@gmail.com>
2023-02-24 00:13:52 +0700

    -add: lightweight linq

---
+++ Internals/Error.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Runtime.CompilerServices;
+
+namespace Pancake
+{
+    public static class Error
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void ThrowArgumentNullException<T>(this T value, string paramName) where T : class
+        {
+            if (value == null) ThrowArgumentNullExceptionCore(paramName);
+        }
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        private static void ThrowArgumentNullExceptionCore(string paramName) { throw new ArgumentNullException(paramName); }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static Exception ArgumentOutOfRange(string paramName) { return new ArgumentOutOfRangeException(paramName); }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static Exception NoElements() { return new InvalidOperationException("Source sequence doesn't contain any elements."); }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static Exception MoreThanOneElement() { return new InvalidOperationException("Source sequence contains more than one element."); }
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        public static void ThrowArgumentException(string message) { throw new ArgumentException(message); }
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        public static void ThrowNotYetCompleted() { throw new InvalidOperationException("Not yet completed."); }
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        public static T ThrowNotYetCompleted<T>() { throw new InvalidOperationException("Not yet completed."); }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void ThrowWhenContinuationIsAlreadyRegistered<T>(T continuationField) where T : class
+        {
+            if (continuationField != null) ThrowInvalidOperationExceptionCore("continuation is already registered.");
+        }
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        private static void ThrowInvalidOperationExceptionCore(string message) { throw new InvalidOperationException(message); }
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        public static void ThrowOperationCanceledException() { throw new OperationCanceledException(); }
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        public static Exception ArgumentNull(string s) { return new ArgumentNullException(s); }
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        public static Exception MoreThanOneMatch() { return new InvalidOperationException("Sequence contains more than one matching element"); }
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        public static Exception NoMatch() { return new InvalidOperationException("Sequence contains no matching element"); }
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        public static Exception NotFound(string key) { return new Exception($"No data saved with key: {key}"); }
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        public static Exception NotSupported() { return new NotSupportedException(); }
+    }
+}
---
+++ Runtime/Linq/Distinct.cs
@@ -0,0 +1,79 @@
+﻿namespace Pancake.Linq
+{
+    public static partial class L
+    {
+        public static System.Collections.Generic.IEnumerable<TSource> Distinct<TSource>(this System.Collections.Generic.IEnumerable<TSource> source)
+        {
+            return System.Linq.Enumerable.Distinct(source);
+        }
+
+        public static System.Collections.Generic.IEnumerable<TSource> Distinct<TSource>(this System.Collections.Generic.IEnumerable<TSource> source, System.Collections.Generic.IEqualityComparer<TSource> comparer)
+        {
+            return System.Linq.Enumerable.Distinct(source, comparer);
+        }
+
+        public static System.Linq.ParallelQuery<TSource> Distinct<TSource>(this System.Linq.ParallelQuery<TSource> source)
+        {
+            return System.Linq.ParallelEnumerable.Distinct(source);
+        }
+
+        public static System.Linq.ParallelQuery<TSource> Distinct<TSource>(this System.Linq.ParallelQuery<TSource> source, System.Collections.Generic.IEqualityComparer<TSource> comparer)
+        {
+            return System.Linq.ParallelEnumerable.Distinct(source, comparer);
+        }
+        
+        /// <summary>
+        /// Returns distinct elements from the given sequence using the default equality comparer
+        /// to compare values projected by <paramref name="projection"/>.
+        /// </summary>
+        /// <typeparam name="TSource">The type of the elements of <paramref name="source"/>.</typeparam>
+        /// <typeparam name="TResult">The type of the projected value for each element of the sequence.</typeparam>
+        /// <param name="source">The sequence.</param>
+        /// <param name="projection">The projection that is applied to each element to retrieve the value which is being compared.</param>
+        /// <returns>A sequence of elements whose projected values are distinct.</returns>
+        public static System.Collections.Generic.IEnumerable<TSource> Distinct<TSource, TResult>(this System.Collections.Generic.IEnumerable<TSource> source, System.Func<TSource, TResult> projection)
+        {
+            if (source == null) throw Error.ArgumentNull("source");
+            if (projection == null) Error.ArgumentNull("projection");
+
+            return DistinctIterator(source, projection, System.Collections.Generic.EqualityComparer<TResult>.Default);
+        }
+
+        /// <summary>
+        /// Returns distinct elements from the given sequence using the specified equality comparer
+        /// to compare values projected by <paramref name="projection"/>.
+        /// </summary>
+        /// <typeparam name="TSource">The type of the elements of <paramref name="source"/>.</typeparam>
+        /// <typeparam name="TResult">The type of the projected value for each element of the sequence.</typeparam>
+        /// <param name="source">The sequence.</param>
+        /// <param name="projection">The projection that is applied to each element to retrieve the value which is being compared.</param>
+        /// <param name="equalityComparer">The equality comparer to use for comparing the projected values.</param>
+        /// <returns>A sequence of elements whose projected values are considered distinct by the specified equality comparer.</returns>
+        public static System.Collections.Generic.IEnumerable<TSource> Distinct<TSource, TResult>(this System.Collections.Generic.IEnumerable<TSource> source, System.Func<TSource, TResult> projection, System.Collections.Generic.IEqualityComparer<TResult> equalityComparer)
+        {
+            if (source == null) throw Error.ArgumentNull("source");
+            if (projection == null) Error.ArgumentNull("projection");
+            if (equalityComparer == null) Error.ArgumentNull("equalityComparer");
+
+            return DistinctIterator(source, projection, equalityComparer);
+        }
+
+        private static System.Collections.Generic.IEnumerable<TSource> DistinctIterator<TSource, TResult>(System.Collections.Generic.IEnumerable<TSource> source, System.Func<TSource, TResult> projection, System.Collections.Generic.IEqualityComparer<TResult> equalityComparer)
+        {
+            var alreadySeenValues = new System.Collections.Generic.HashSet<TResult>(equalityComparer);
+
+            foreach (var element in source)
+            {
+                var value = projection(element);
+
+                if (alreadySeenValues.Contains(value))
+                {
+                    continue;
+                }
+
+                yield return element;
+                alreadySeenValues.Add(value);
+            }
+        }
+    }
+}
