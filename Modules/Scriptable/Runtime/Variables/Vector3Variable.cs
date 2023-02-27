865ac162d2df4655185ec52a9ac6dd09e703d163
yenmoc <phongsoyenmoc.diep@gmail.com>
2023-02-28 01:39:19 +0700

    -add: scriptable vector3 variable

---
+++ Modules/Scriptable/Runtime/Variables/Vector3Variable.cs
@@ -0,0 +1,21 @@
+﻿namespace Pancake.Scriptable
+{
+    using UnityEngine;
+
+    [CreateAssetMenu(fileName = "scriptable_variable_vector3.asset", menuName = "Pancake/ScriptableVariables/vector3")]
+    [System.Serializable]
+    public class Vector3Variable : ScriptableVariable<Vector3>
+    {
+        public override void Save()
+        {
+            Data.Save(Id, Value);
+            base.Save();
+        }
+
+        public override void Load()
+        {
+            Value = Data.Load(Id, initialValue);
+            base.Load();
+        }
+    }
+}
