3db7ea4a77545a7029889c5970e88d7236ae515d
yenmoc <phongsoyenmoc.diep@gmail.com>
2023-02-28 22:27:36 +0700

    -add: list

---
+++ Modules/Scriptable/Runtime/Lists/ScriptableListGameObject.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace Pancake.Scriptable
+{
+    [CreateAssetMenu(fileName = "scriptable_list_gameObject.asset", menuName = "Pancake/ScriptableLists/GameObject")]
+    public class ScriptableListGameObject : ScriptableList<GameObject>
+    {
+    }
+}
---
+++ Modules/Scriptable/Runtime/Lists/ScriptableListVector3.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace Pancake.Scriptable
+{
+    [CreateAssetMenu(fileName = "scriptable_list_vector3.asset", menuName = "Pancake/ScriptableLists/Vector3")]
+    public class ScriptableListVector3 : ScriptableList<Vector3>
+    {
+    }
+}
