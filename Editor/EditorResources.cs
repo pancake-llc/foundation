b1f6339bb6e10b560b7c2cc872ac53b9c551c80b
yenmoc <phongsoyenmoc.diep@gmail.com>
2023-03-03 15:16:15 +0700

    -update: editor resource

--- Editor/EditorResources.cs
+++ Editor/EditorResources.cs
@@ -4,7 +4,7 @@ namespace PancakeEditor
 {
     internal static class EditorResources
     {
-        private const string RELATIVE_PATH = "Editor/Skins/Icons";
+        private const string RELATIVE_PATH = "Editor/Misc/Icons";
 
         internal static Texture2D ContentBackground => Editor.FindAssetWithPath<Texture2D>("content_bg.png", RELATIVE_PATH);
         internal static Texture2D ContentBackgroundDark => Editor.FindAssetWithPath<Texture2D>("content_bg_dark.png", RELATIVE_PATH);
@@ -13,6 +13,10 @@ namespace PancakeEditor
         internal static Texture2D EvenBackground => Editor.FindAssetWithPath<Texture2D>("even_bg.png", RELATIVE_PATH);
         internal static Texture2D EvenBackgroundBlue => Editor.FindAssetWithPath<Texture2D>("even_bg_select.png", RELATIVE_PATH);
         internal static Texture2D EvenBackgroundDark => Editor.FindAssetWithPath<Texture2D>("even_bg_dark.png", RELATIVE_PATH);
-
+        internal static Texture2D ScriptableEvent => Editor.FindAssetWithPath<Texture2D>("scriptable_event.png", RELATIVE_PATH);
+        internal static Texture2D ScriptableEventListener => Editor.FindAssetWithPath<Texture2D>("scriptable_event_listener.png", RELATIVE_PATH);
+        internal static Texture2D ScriptableList => Editor.FindAssetWithPath<Texture2D>("scriptable_list.png", RELATIVE_PATH);
+        internal static Texture2D ScriptablePlaymodeResetter => Editor.FindAssetWithPath<Texture2D>("scriptable_playmode_resetter.png", RELATIVE_PATH);
+        internal static Texture2D ScriptableVariable => Editor.FindAssetWithPath<Texture2D>("scriptable_variable.png", RELATIVE_PATH);
     }
 }
