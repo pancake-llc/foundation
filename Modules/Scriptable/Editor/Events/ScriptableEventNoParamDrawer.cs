b9d1574d6ffe5257c546a9928cbb91a7717589d1
yenmoc <phongsoyenmoc.diep@gmail.com>
2023-03-03 14:37:13 +0700

    -add: event no param drawer

---
+++ Modules/Scriptable/Editor/Events/ScriptableEventNoParamDrawer.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using Pancake.Scriptable;
+using UnityEditor;
+using UnityEngine;
+
+namespace PancakeEditor.Scriptable
+{
+    [CustomEditor(typeof(ScriptableEventNoParam))]
+    public class ScriptableEventNoParamDrawer : UnityEditor.Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            DrawDefaultInspector();
+
+            if (GUILayout.Button("Raise"))
+            {
+                var eventNoParam = (ScriptableEventNoParam) target;
+                eventNoParam.Raise();
+            }
+
+            if (!EditorApplication.isPlaying) return;
+
+            Editor.DrawLine();
+
+            var container = (IDrawObjectsInInspector) target;
+            var gameObjects = container.GetAllObjects();
+
+            if (gameObjects.Count > 0) DisplayAll(gameObjects);
+        }
+
+        private void DisplayAll(List<Object> gameObjects)
+        {
+            GUILayout.Space(15);
+            var title = $"Listener Amount : {gameObjects.Count}";
+            GUILayout.BeginVertical(title, "window");
+            foreach (var obj in gameObjects)
+            {
+                Editor.DrawSelectableObject(obj, new[] {obj.name, "Select"});
+            }
+
+            GUILayout.EndVertical();
+        }
+    }
+}
