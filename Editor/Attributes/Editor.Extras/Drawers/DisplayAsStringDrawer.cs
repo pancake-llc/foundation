5896a3600efc29c22925308fa3f8aecbdbe86f4f
yenmoc <phongsoyenmoc.diep@gmail.com>
2023-03-13 00:19:40 +0700

    -add: display as string attribute

---
+++ Editor/Attributes/Editor.Extras/Drawers/DisplayAsStringDrawer.cs
@@ -0,0 +1,24 @@
+﻿using Pancake.Attribute;
+using PancakeEditor.Attribute;
+using UnityEditor;
+using UnityEngine;
+
+[assembly: RegisterAttributeDrawer(typeof(DisplayAsStringDrawer), DrawerOrder.Decorator, ApplyOnArrayElement = true)]
+
+namespace PancakeEditor.Attribute
+{
+    public class DisplayAsStringDrawer : AttributeDrawer<DisplayAsStringAttribute>
+    {
+        public override float GetHeight(float width, Property property, InspectorElement next) { return EditorGUIUtility.singleLineHeight; }
+
+        public override void OnGUI(Rect position, Property property, InspectorElement next)
+        {
+            var value = property.Value;
+            var text = value != null ? value.ToString() : "Null";
+
+            var controlId = GUIUtility.GetControlID(FocusType.Passive);
+            position = EditorGUI.PrefixLabel(position, controlId, property.DisplayNameContent);
+            GUI.Label(position, text);
+        }
+    }
+}
