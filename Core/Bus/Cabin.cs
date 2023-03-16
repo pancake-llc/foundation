21ff006c8837c0d6975729a61002de053489b494
yenmoc <phongsoyenmoc.diep@gmail.com>
2023-03-17 01:52:51 +0700

    -add: cabin

---
+++ Core/Bus/Cabin.cs
@@ -0,0 +1,10 @@
+﻿namespace Pancake
+{
+    /// <summary>
+    /// Is the place to store the seats of the passengers on the Bus
+    /// </summary>
+    public static class Cabin
+    {
+       
+    }
+}
