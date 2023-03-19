be238672a84d71545335224510da9feafcbe1f32
yenmoc <phongsoyenmoc.diep@gmail.com>
2023-03-19 21:40:27 +0700

    -add: developerDescription to scriptable base

--- Modules/Scriptable/Runtime/Variables/ScriptableBase.cs
+++ Modules/Scriptable/Runtime/Variables/ScriptableBase.cs
@@ -8,5 +8,6 @@ namespace Pancake.Scriptable
     [Searchable]
     public class ScriptableBase : ScriptableObject
     {
+        [SerializeField, TextArea(3, 6)] private string developerDescription;
     }
 }
