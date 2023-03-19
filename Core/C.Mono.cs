26d10dca30e2f7969c4688250a9b547d9dbbba90
yenmoc <phongsoyenmoc.diep@gmail.com>
2023-03-19 17:47:24 +0700

    -add: coroutine handle

---
+++ Core/C.Mono.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Pancake
+{
+    public static partial class C
+    {
+        public static CoroutineHandle RunCoroutine(this MonoBehaviour owner, IEnumerator coroutine) { return new CoroutineHandle(owner, coroutine); }
+    }
+}
--- Core/C.Transform.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Pancake
-{
-    public static partial class C
-    {
-        
-    }
-}
