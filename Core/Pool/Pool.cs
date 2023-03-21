39a6df329400113838d3b0c760e1a348a6b2c5e8
yenmoc <phongsoyenmoc.diep@gmail.com>
2023-03-21 22:18:51 +0700

    -add: pool generric

--- Core/Pool/Pool.cs
+++ Core/Pool/Pool.cs
@@ -1,85 +1,93 @@
-using System;
 using System.Collections.Generic;
 using Pancake.Attribute;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 namespace Pancake
 {
+    /// <summary>
+    /// A generic pool that generates members of type T on-demand via a factory.
+    /// </summary>
+    /// <typeparam name="T">Specifies the type of elements to pool.</typeparam>
     [EditorIcon("script_pool")]
-    [Searchable]
-    [CreateAssetMenu(fileName = "Pool", menuName = "Pancake/Misc/Pool")]
-    public class Pool : ScriptableObject
+    public abstract class Pool<T> : ScriptableObject, IPool<T>
     {
         [SerializeField, TextArea(3, 6)] private string developerDescription;
-        [SerializeField] private GameObject prefab;
-        [SerializeField] private int size;
-        [SerializeField] private bool dontDestroyOnLoad;
-        
-        private event Action<string> OnSceneUnloadedEvent;
-        private bool _initialize;
-        internal LinkedList<PoolMember> PoolMembers { get; private set; }
-
-        private void OnEnable() { Reset(); }
-
-        private void Awake() { hideFlags = HideFlags.DontUnloadUnusedAsset; }
 
         /// <summary>
-        /// Init the pool once time
+        /// The factory which will be used to create <typeparamref name="T"/> on demand.
         /// </summary>
-        public void Init()
+        public abstract IFactory<T> Factory { get; set; }
+
+        protected bool IsPrewarmed { get; set; }
+        protected readonly Stack<T> container = new Stack<T>();
+
+        /// <summary>
+        /// Prewarms the pool with a <paramref name="size"/> of <typeparamref name="T"/>.
+        /// </summary>
+        /// <param name="size">The number of members to create as a part of this pool.</param>
+        /// <remarks>NOTE: This method can be called at any time, but only once for the lifetime of the pool.</remarks>
+        public virtual void Prewarm(int size)
         {
-            if (!Application.isPlaying || _initialize) return;
-            _initialize = true;
-            PoolMembers = new LinkedList<PoolMember>();
+            if (IsPrewarmed)
+            {
+                Debug.LogWarning($"Pool {name} has already been prewarmed.");
+                return;
+            }
+
             for (var i = 0; i < size; i++)
             {
-                PoolMembers.AddLast(Create());
+                container.Push(Create());
             }
 
-            SceneManager.sceneUnloaded -= OnsceneUnloaded;
-            SceneManager.sceneUnloaded += OnsceneUnloaded;
+            IsPrewarmed = true;
         }
 
-        public PoolMember Get()
+        /// <summary>
+        /// Requests a <typeparamref name="T"/> from this pool.
+        /// </summary>
+        /// <returns>The requested <typeparamref name="T"/>.</returns>
+        public virtual T Request() { return container.Count > 0 ? container.Pop() : Create(); }
+
+        /// <summary>
+        /// Batch requests a <typeparamref name="T"/> collection from this pool.
+        /// </summary>
+        /// <param name="count"></param>
+        /// <returns>A <typeparamref name="T"/> collection.</returns>
+        public virtual IEnumerable<T> Request(int count)
         {
-            if (PoolMembers.Count > 0)
+            var members = new List<T>(count);
+            for (var i = 0; i < count; i++)
             {
-                var member = PoolMembers.First.Value;
-                PoolMembers.RemoveFirst();
-                member.SetContext(SceneManager.GetActiveScene().name);
-                return member;
+                members.Add(Request());
             }
 
-            return Create();
+            return members;
         }
 
-        public void Reset()
+        /// <summary>
+        /// Returns a <typeparamref name="T"/> to the pool.
+        /// </summary>
+        /// <param name="member">The <typeparamref name="T"/> to return.</param>
+        public virtual void Return(T member) { container.Push(member); }
+
+        /// <summary>
+        /// Returns a <typeparamref name="T"/> collection to the pool.
+        /// </summary>
+        /// <param name="members">The <typeparamref name="T"/> collection to return.</param>
+        public virtual void Return(IEnumerable<T> members)
         {
-            if (PoolMembers != null)
+            foreach (var member in members)
             {
-                foreach (var member in PoolMembers)
-                {
-                    member.Return();
-                }
-
-                PoolMembers = null;
+                Return(member);
             }
-
-            OnSceneUnloadedEvent = null;
-            _initialize = false;
         }
 
-        private PoolMember Create()
+        protected virtual T Create() { return Factory.Create(); }
+
+        public virtual void OnDisable()
         {
-            var poolMember = Instantiate(prefab, null).AddComponent<PoolMember>();
-            if (dontDestroyOnLoad) DontDestroyOnLoad(poolMember);
-            poolMember.Init(this, false, RegisterOnScenedUnloaded, UnregisterOnScenedUnloaded);
-            return poolMember;
+            container.Clear();
+            IsPrewarmed = false;
         }
-
-        private void OnsceneUnloaded(Scene scene) { OnSceneUnloadedEvent?.Invoke(scene.name); }
-        private void UnregisterOnScenedUnloaded(Action<string> action) { OnSceneUnloadedEvent -= action; }
-        private void RegisterOnScenedUnloaded(Action<string> action) { OnSceneUnloadedEvent += action; }
     }
 }
