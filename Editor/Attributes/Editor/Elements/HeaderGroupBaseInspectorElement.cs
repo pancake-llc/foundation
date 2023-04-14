17d2d7c65ed0f5c773ecabb85daf77a8103e0cf8
yenmoc <phongsoyenmoc.diep@gmail.com>
2023-04-14 09:34:43 +0700

    -fix: height header group

--- Editor/Attributes/Editor/Elements/HeaderGroupBaseInspectorElement.cs
+++ Editor/Attributes/Editor/Elements/HeaderGroupBaseInspectorElement.cs
@@ -13,13 +13,13 @@ namespace PancakeEditor.Attribute
 
         protected virtual float GetHeaderHeight(float width) { return 22; }
 
-        protected virtual float GetContentHeight(float width) { return base.GetHeight(width) + InsetTop + InsetBottom; }
+        protected virtual float GetContentHeight(float width) { return base.GetHeight(width); }
 
         protected virtual void DrawHeader(Rect position) { }
 
         protected virtual void DrawContent(Rect position) { base.OnGUI(position); }
 
-        public sealed override float GetHeight(float width) { return GetContentHeight(width) + GetHeaderHeight(width); }
+        public sealed override float GetHeight(float width) { return GetContentHeight(width) + GetHeaderHeight(width) + InsetTop + InsetBottom; }
 
         public sealed override void OnGUI(Rect position)
         {
