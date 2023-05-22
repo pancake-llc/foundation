348422d641226f1e9fa83325c0099a585a5663bb
yenmoc <phongsoyenmoc.diep@gmail.com>
2023-05-22 17:25:17 +0700

    -update: adsetting

--- Modules/Advertising/AdSettings.cs
+++ Modules/Advertising/AdSettings.cs
@@ -1,5 +1,8 @@
 using System.Collections.Generic;
 using Pancake.Apex;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 using UnityEngine;
 
 
@@ -15,8 +18,9 @@ namespace Pancake.Monetization
         [Range(5, 100), SerializeField] private float adLoadingInterval = 15f;
         [HorizontalLine] [SerializeField] private EAdNetwork currentNetwork = EAdNetwork.Applovin;
 
-        [Header("[admob]")] [SerializeField, Label("Test Mode")]
-        private bool admobEnableTestMode;
+
+#if PANCAKE_ADVERTISING && PANCAKE_ADMOB
+        [Header("[admob]")] [SerializeField, Label("Test Mode")] private bool admobEnableTestMode;
 
         [SerializeField, Array, ShowIf(nameof(admobEnableTestMode)), Label("Devices Test")]
         private List<string> admobDevicesTest;
@@ -27,6 +31,17 @@ namespace Pancake.Monetization
         [SerializeField, Label("Inter Rewarded")] private AdmobRewardInterVariable admobRewardInter;
         [HorizontalLine] [SerializeField, Label("App Open")] private AdmobAppOpenVariable admobAppOpen;
 
+        public List<string> AdmobDevicesTest => admobDevicesTest;
+        public bool AdmobEnableTestMode => admobEnableTestMode;
+        public AdmobBannerVariable AdmobBanner => admobBanner;
+        public AdmobInterVariable AdmobInter => admobInter;
+        public AdmobRewardVariable AdmobReward => admobReward;
+        public AdmobRewardInterVariable AdmobRewardInter => admobRewardInter;
+        public AdmobAppOpenVariable AdmobAppOpen => admobAppOpen;
+#endif
+
+
+#if PANCAKE_ADVERTISING && PANCAKE_APPLOVIN
         [Header("[applovin]")] [SerializeField, TextArea] private string sdkKey;
         [SerializeField, Label("Banner")] private AdmobBannerVariable applovinBanner;
         [SerializeField, Label("Interstitial")] private AdmobInterVariable applovinInter;
@@ -36,18 +51,46 @@ namespace Pancake.Monetization
         [SerializeField, Label("Age Restricted")] private bool applovinEnableAgeRestrictedUser;
         [SerializeField, Label("Request Ad After Hidden")] private bool applovinEnableRequestAdAfterHidden = true;
         [SerializeField, Label("Max Ad Review")] private bool applovinEnableMaxAdReview;
+            
+        public bool ApplovinEnableRequestAdAfterHidden => applovinEnableRequestAdAfterHidden;
+#endif
 
 
         public float AdCheckingInterval => adCheckingInterval;
         public float AdLoadingInterval => adLoadingInterval;
         public EAdNetwork CurrentNetwork { get => currentNetwork; set => currentNetwork = value; }
-        public List<string> AdmobDevicesTest => admobDevicesTest;
-        public bool AdmobEnableTestMode => admobEnableTestMode;
-        public bool ApplovinEnableRequestAdAfterHidden => applovinEnableRequestAdAfterHidden;
-        public AdmobBannerVariable AdmobBanner => admobBanner;
-        public AdmobInterVariable AdmobInter => admobInter;
-        public AdmobRewardVariable AdmobReward => admobReward;
-        public AdmobRewardInterVariable AdmobRewardInter => admobRewardInter;
-        public AdmobAppOpenVariable AdmobAppOpen => admobAppOpen;
+
+
+#if UNITY_EDITOR
+
+        [ShowIf(nameof(IsAdmobSdkNotImported))]
+        [SerializeMethod]
+        [Color(0,
+            0.9f,
+            0,
+            1,
+            Target = ColorTarget.Background)]
+        [HorizontalGroup("button-install")]
+        private void InstallAdmobSdk()
+        {
+            
+        }
+
+        [ShowIf(nameof(IsAdmobSdkImported))]
+        [SerializeMethod]
+        [Color(1,
+            0,
+            0,
+            0.88f,
+            Target = ColorTarget.Background)]
+        [HorizontalGroup("button-uninstall")]
+        private void UnInstallAdmobSdk() { }
+
+        private bool IsAdmobSdkImported() { return UnityEditor.AssetDatabase.FindAssets("l:gvhp_exportpath-GoogleMobileAds/GoogleMobileAds.dll").Length >= 1; }
+
+        private bool IsAdmobSdkNotImported() => !IsAdmobSdkImported();
+
+        private void InstallApplovinSdk() { }
+#endif
     }
 }
