c2af3adc32af1b6308169d6f47d0620496803028
yenmoc <phongsoyenmoc.diep@gmail.com>
2023-05-22 14:43:56 +0700

    -update: add RequireNullCheck for Data Save and Load

--- Core/Runtime/Data.cs
+++ Core/Runtime/Data.cs
@@ -13,7 +13,6 @@ namespace Pancake
     public static class Data
     {
         private static bool isInitialized;
-        private static string path = string.Empty;
         private static int profile;
         private static Dictionary<string, string> datas;
         private const int INIT_SIZE = 64;
@@ -28,7 +27,6 @@ namespace Pancake
             if (isInitialized) return;
             isInitialized = true;
 
-            GeneratePath();
             LoadAll();
             App.AddFocusCallback(OnApplicationFocus);
         }
@@ -39,13 +37,19 @@ namespace Pancake
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         private static T Deserialize<T>(string json) { return JsonConvert.DeserializeObject<T>(json); }
 
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        private static void GeneratePath() { path = Path.Combine(Application.persistentDataPath, $"masterdata_{profile}.json"); }
-
         private static void OnApplicationFocus(bool focus)
         {
             if (!focus) SaveAll();
         }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private static void RequireNullCheck()
+        {
+            if (datas == null) LoadAll();
+            if (datas == null) throw new NullReferenceException();
+        }
+
+        private static string GetPath => Path.Combine(Application.persistentDataPath, $"masterdata_{profile}.json");
 
         #endregion
 
@@ -60,7 +64,6 @@ namespace Pancake
 
             SaveAll();
             Data.profile = profile;
-            GeneratePath();
             LoadAll();
         }
 
@@ -70,50 +73,43 @@ namespace Pancake
             OnSaveEvent?.Invoke();
 
             string json = Serialize(datas);
-            File.WriteAllText(path, json);
+            File.WriteAllText(GetPath, json);
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void LoadAll()
         {
-            if (!File.Exists(path))
+            if (!File.Exists(GetPath))
             {
-                var stream = File.Create(path);
+                var stream = File.Create(GetPath);
                 stream.Close();
             }
 
-            var json = File.ReadAllText(path);
+            string json = File.ReadAllText(GetPath);
             datas = Deserialize<Dictionary<string, string>>(json) ?? new Dictionary<string, string>(INIT_SIZE);
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="default">If value of <paramref name="key"/> can not be found or empty! will return the default value of data type!</param>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static T Load<T>(string key, T @default = default)
         {
-            if (datas == null)
-            {
-                GeneratePath();
-                LoadAll();
-            }
+            RequireNullCheck();
 
-            if (datas == null) throw new NullReferenceException();
-            
             datas.TryGetValue(key, out string value);
-            if (value != null && !string.IsNullOrEmpty(value)) return Deserialize<T>(value);
-            //Debug.Log($"Value of key <color=#52D5F2>'{key}'</color> can not be found or empty! So will return the default value of data type!");
-            return @default;
+            return !string.IsNullOrEmpty(value) ? Deserialize<T>(value) : @default;
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static bool TryLoad<T>(string key, out T data)
         {
-            if (datas == null)
-            {
-                GeneratePath();
-                LoadAll();
-            }
+            RequireNullCheck();
 
-            if (datas == null) throw new NullReferenceException();
-            
             bool hasKey;
             if (datas.TryGetValue(key, out string value))
             {
@@ -132,6 +128,8 @@ namespace Pancake
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void Save<T>(string key, T data)
         {
+            RequireNullCheck();
+
             if (datas.TryGetValue(key, out string _))
             {
                 datas[key] = Serialize(data);
