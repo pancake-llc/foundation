381ba4470fe0d466c0265cd2c7758206edebb9ce
yenmoc <phongsoyenmoc.diep@gmail.com>
2023-05-28 15:31:13 +0700

    -update

--- Core/Runtime/Invariant.cs
+++ Core/Runtime/Invariant.cs
@@ -4,11 +4,5 @@ namespace Pancake
     {
         public const string USER_AGREE_PRIVACY_KEY = "user_agree_privacy";
         public const string FIRST_OPEN_TIMESTAMP_KEY = "first_open_timestamp";
-        
-        public const string LAUNCHER_SCENE = "[C] launcher";
-        public const string PERSISTENT_SCENE = "[C] persistent";
-        public const string MENU_SCENE = "[C] menu";
-        public const string GAMEPLAY_SCENE = "[C] gameplay";
-        
     }
 }
