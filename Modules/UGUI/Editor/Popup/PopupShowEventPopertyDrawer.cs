de638d9cedf39cf71e1f0763b5b8f0838ce4dfae
yenmoc <phongsoyenmoc.diep@gmail.com>
2023-05-30 10:35:41 +0700

    -add: drawer for popup show event

---
+++ Modules/UGUI/Editor/Popup/PopupShowEventPopertyDrawer.cs
@@ -0,0 +1,30 @@
+﻿using Pancake.Scriptable;
+using Pancake.ScriptableEditor;
+using Pancake.UI;
+using UnityEditor;
+using UnityEngine;
+
+namespace PancakeEditor
+{
+    [CustomPropertyDrawer(typeof(PopupShowEvent), true)]
+    public class PopupShowEventPopertyDrawer : ScriptableBasePropertyDrawer
+    {
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            EditorGUI.BeginProperty(position, label, property);
+
+            var targetObject = property.objectReferenceValue;
+            if (targetObject == null)
+            {
+                DrawIfNull(position, property, label);
+                return;
+            }
+
+            DrawIfNotNull(position, property, label, property.objectReferenceValue);
+
+            EditorGUI.EndProperty();
+        }
+
+        protected override void DrawShortcut(Rect position, SerializedProperty property, Object targetObject) { }
+    }
+}
