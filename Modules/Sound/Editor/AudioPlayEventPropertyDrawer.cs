0079a4d4868778924a0e560d6fc0cad215983992
yenmoc <phongsoyenmoc.diep@gmail.com>
2023-05-30 10:34:55 +0700

    -add: drawer for audio play event

---
+++ Modules/Sound/Editor/AudioPlayEventPropertyDrawer.cs
@@ -0,0 +1,28 @@
+﻿using Pancake.ScriptableEditor;
+using UnityEditor;
+using UnityEngine;
+
+namespace Pancake.Sound
+{
+    [CustomPropertyDrawer(typeof(AudioPlayEvent), true)]
+    public class AudioPlayEventPropertyDrawer : ScriptableBasePropertyDrawer
+    {
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            EditorGUI.BeginProperty(position, label, property);
+
+            var targetObject = property.objectReferenceValue;
+            if (targetObject == null)
+            {
+                DrawIfNull(position, property, label);
+                return;
+            }
+
+            DrawIfNotNull(position, property, label, property.objectReferenceValue);
+
+            EditorGUI.EndProperty();
+        }
+
+        protected override void DrawShortcut(Rect position, SerializedProperty property, Object targetObject) { }
+    }
+}
