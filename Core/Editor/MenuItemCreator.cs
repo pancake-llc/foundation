1cfd9a91429ea44b1f12e3223d15f37b85eeab9e
yenmoc <phongsoyenmoc.diep@gmail.com>
2023-05-30 10:37:51 +0700

    -update: menu item create popup

--- Core/Editor/MenuItemCreator.cs
+++ Core/Editor/MenuItemCreator.cs
@@ -10,7 +10,7 @@ namespace PancakeEditor
     public static class MenuItemCreator
     {
         [MenuItem("GameObject/Pancake/UIButton", false, 1000)]
-        public static void CreateUniButtonEmpty()
+        public static void CreateUIButtonEmpty()
         {
             var button = CreateObject<UIButton>("Button");
             Undo.RegisterCreatedObjectUndo(button.gameObject, "Create UIButton");
@@ -26,12 +26,20 @@ namespace PancakeEditor
         }
 
         [MenuItem("GameObject/Pancake/UIButton (TMP)", false, 1000)]
-        private static void AddUniButtonTMP()
+        private static void CreateUIButtonTMP()
         {
             var button = CreateObject<UIButtonTMP>("Button");
             Undo.RegisterCreatedObjectUndo(button.gameObject, "Create UIButton TMP");
             SetupUIButton(button);
             Selection.activeTransform = button;
+        }
+
+        [MenuItem("GameObject/Pancake/UIPopup", false, 1000)]
+        public static void CreateUIPopupEmpty()
+        {
+            var popup = CreateUIPopupObject();
+            Undo.RegisterCreatedObjectUndo(popup.gameObject, "Create UIPopup");
+            Selection.activeTransform = popup;
         }
 
         [MenuItem("GameObject/Pancake/UI Set Native Size + Pivot", false, 1000)]
@@ -61,7 +69,63 @@ namespace PancakeEditor
                 }
             }
         }
+        
+        private static RectTransform CreateUIPopupObject()
+        {
+            // find canvas in scene
+            var allCanvases = (Canvas[]) Object.FindObjectsOfType(typeof(Canvas));
+            if (allCanvases.Length > 0)
+            {
+                if (Selection.activeTransform == null) return CreateUniPopup(allCanvases[0].transform);
 
+                for (int i = 0; i < allCanvases.Length; i++)
+                {
+                    if (!Selection.activeTransform.IsChildOf(allCanvases[i].transform)) continue;
+                    return CreateUniPopup(Selection.activeTransform);
+                }
+
+                return CreateUniPopup(allCanvases[0].transform);
+            }
+
+            var canvas = CreateCanvas();
+            return CreateUniPopup(canvas.transform);
+        }
+
+        private static RectTransform CreateUniPopup(Transform parent)
+        {
+            var popup = CreateEmptyRectTransformObject(parent, "popup_basic");
+            popup.gameObject.AddComponent<Canvas>().overrideSorting = true;
+            popup.gameObject.AddComponent<GraphicRaycaster>();
+            popup.gameObject.AddComponent<CanvasGroup>();
+            popup.anchorMin = Vector2.zero;
+            popup.anchorMax = Vector2.one;
+            popup.pivot = new Vector2(0.5f, 0.5f);
+            popup.sizeDelta = Vector2.zero;
+            popup.anchoredPosition3D = Vector3.zero;
+            popup.localPosition = Vector3.zero;
+
+            var background = CreateEmptyRectTransformObject(popup.transform, "background");
+            background.anchorMin = Vector2.zero;
+            background.anchorMax = Vector2.one;
+            background.pivot = new Vector2(0.5f, 0.5f);
+            background.sizeDelta = Vector2.zero;
+            background.anchoredPosition3D = Vector3.zero;
+            background.localPosition = Vector3.zero;
+            var img = background.gameObject.AddComponent<Image>();
+            img.color = new Color(0f, 0f, 0f, 0.78f);
+
+            var container = CreateEmptyRectTransformObject(popup.transform, "container");
+            container.gameObject.AddComponent<Image>().GetComponent<RectTransform>().sizeDelta = new Vector2(800, 500);
+
+            var button = CreateObjectWithComponent<UIButton>(container.transform, "button_close");
+            button.gameObject.layer = LayerMask.NameToLayer("UI");
+            button.sizeDelta = new Vector2(80, 80);
+            button.anchorMax = Vector2.one;
+            button.anchorMin = Vector2.one;
+            button.anchoredPosition = Vector2.zero;
+            return popup;
+        }
+        
         private static RectTransform CreateEmptyRectTransformObject(Transform parent, string name)
         {
             var obj = new GameObject(name);
