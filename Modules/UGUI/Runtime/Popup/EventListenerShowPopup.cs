4c9d907896fe8782ccd82de510f3e88327a00873
yenmoc <phongsoyenmoc.diep@gmail.com>
2023-05-30 08:46:56 +0700

    -add: popup event

---
+++ Modules/UGUI/Runtime/Popup/EventListenerShowPopup.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections.Generic;
+using Pancake.Scriptable;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Pancake.UI
+{
+    public class EventListenerShowPopup : EventListenerBase
+    {
+        [System.Serializable]
+        public struct EventResponse
+        {
+            public PopupShowEvent popupShowEvent;
+            public UnityEvent response;
+        }
+
+        [SerializeField] private EventResponse[] eventResponses;
+
+        private Dictionary<PopupShowEvent, UnityEvent> _dictionary = new Dictionary<PopupShowEvent, UnityEvent>();
+
+        protected override void ToggleRegistration(bool toggle)
+        {
+            for (var i = 0; i < eventResponses.Length; i++)
+            {
+                if (toggle)
+                {
+                    eventResponses[i].popupShowEvent.RegisterListener(this);
+
+                    _dictionary.TryAdd(eventResponses[i].popupShowEvent, eventResponses[i].response);
+                }
+                else
+                {
+                    eventResponses[i].popupShowEvent.UnregisterListener(this);
+                    if (_dictionary.ContainsKey(eventResponses[i].popupShowEvent)) _dictionary.Remove(eventResponses[i].popupShowEvent);
+                }
+            }
+        }
+
+        public void OnEventRaised(PopupShowEvent eventRaised) { _dictionary[eventRaised].Invoke(); }
+
+        public override bool ContainsCallToMethod(string methodName)
+        {
+            var containsMethod = false;
+            foreach (var r in eventResponses)
+            {
+                var registeredListenerCount = r.response.GetPersistentEventCount();
+
+                for (int i = 0; i < registeredListenerCount; i++)
+                {
+                    if (r.response.GetPersistentMethodName(i) == methodName)
+                    {
+                        var debugText = $"<color=#f75369>{methodName}()</color>";
+                        debugText += " is called by the event: <color=#f75369>";
+                        debugText += r.popupShowEvent.name;
+                        debugText += "</color>";
+                        UnityEngine.Debug.Log(debugText, gameObject);
+                        containsMethod = true;
+                        break;
+                    }
+                }
+            }
+
+            return containsMethod;
+        }
+    }
+}
--- Modules/UGUI/Runtime/Popup/PopupShowEvent.cs
+++ Modules/UGUI/Runtime/Popup/PopupShowEvent.cs
@@ -1,6 +1,8 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using Pancake.Scriptable;
 using UnityEngine;
+using Object = UnityEngine.Object;
 
 namespace Pancake.UI
 {
@@ -9,9 +11,54 @@ namespace Pancake.UI
     [CreateAssetMenu(fileName = "popup_display_chanel.asset", menuName = "Pancake/UI/PopupShowEvent")]
     public class PopupShowEvent : ScriptableEventBase, IDrawObjectsInInspector
     {
+        private readonly List<EventListenerShowPopup> _eventListeners = new List<EventListenerShowPopup>();
+        private readonly List<Object> _listenerObjects = new List<Object>();
+
+        private Action<UIPopup, Transform, Action<UIPopup>> _onRaised;
+
+        public event Action<UIPopup, Transform, Action<UIPopup>> OnRaised
+        {
+            add
+            {
+                _onRaised += value;
+                var listener = value.Target as Object;
+                if (listener != null && !_listenerObjects.Contains(listener)) _listenerObjects.Add(listener);
+            }
+            remove
+            {
+                _onRaised -= value;
+                var listener = value.Target as Object;
+                if (_listenerObjects.Contains(listener)) _listenerObjects.Remove(listener);
+            }
+        }
+
+        public void Raise(UIPopup prefab, Transform parent, Action<UIPopup> callback)
+        {
+            if (!Application.isPlaying) return;
+            
+            for (int i = _eventListeners.Count - 1; i >= 0; i--)
+            {
+                _eventListeners[i].OnEventRaised(this);
+            }
+
+            _onRaised?.Invoke(prefab, parent, callback);
+        }
+
+        public void RegisterListener(EventListenerShowPopup listener)
+        {
+            if (!_eventListeners.Contains(listener)) _eventListeners.Add(listener);
+        }
+
+        public void UnregisterListener(EventListenerShowPopup listener)
+        {
+            if (_eventListeners.Contains(listener)) _eventListeners.Remove(listener);
+        }
+
         public List<Object> GetAllObjects()
         {
-            return null;
+            var allObjects = new List<Object>(_eventListeners);
+            allObjects.AddRange(_listenerObjects);
+            return allObjects;
         }
     }
 }
