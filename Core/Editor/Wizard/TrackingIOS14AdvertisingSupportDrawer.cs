c2381739bcefeff93e2fb4942109a167838406d0
yenmoc <phongsoyenmoc.diep@gmail.com>
2023-06-12 15:22:35 +0700

    -remove: ios 14 ad support setting default using skad 63

--- Core/Editor/Wizard/TrackingIOS14AdvertisingSupportDrawer.cs
+++ Core/Editor/Wizard/TrackingIOS14AdvertisingSupportDrawer.cs
@@ -1,7 +1,4 @@
-﻿using System.IO;
-using Pancake;
-using Pancake.ExLibEditor;
-using Pancake.Tracking;
+﻿using Pancake.ExLibEditor;
 using UnityEditor;
 using UnityEngine;
 
@@ -13,33 +10,6 @@ namespace PancakeEditor
         {
 #if PANCAKE_ATT
             Uniform.DrawInstalled("1.2.0");
-            EditorGUILayout.Space();
-
-            var ios14SupportSetting = Resources.Load<IOS14AdSupportSetting>(nameof(IOS14AdSupportSetting));
-            if (ios14SupportSetting == null)
-            {
-                GUI.enabled = !EditorApplication.isCompiling;
-                GUI.backgroundColor = Uniform.Pink;
-                if (GUILayout.Button("Create iOS 14 Ad Support Setting", GUILayout.Height(40)))
-                {
-                    var setting = ScriptableObject.CreateInstance<IOS14AdSupportSetting>();
-                    const string path = "Assets/_Root/Resources";
-                    if (!Directory.Exists(path)) Directory.CreateDirectory(path);
-                    AssetDatabase.CreateAsset(setting, $"{path}/{nameof(IOS14AdSupportSetting)}.asset");
-                    AssetDatabase.SaveAssets();
-                    AssetDatabase.Refresh();
-                    Debug.Log($"{nameof(IOS14AdSupportSetting).TextColor("#f75369")} was created ad {path}/{nameof(IOS14AdSupportSetting)}.asset");
-                }
-
-                GUI.backgroundColor = Color.white;
-                GUI.enabled = true;
-            }
-            else
-            {
-                var editor = UnityEditor.Editor.CreateEditor(ios14SupportSetting);
-                editor.OnInspectorGUI();
-            }
-
             GUILayout.FlexibleSpace();
             GUI.backgroundColor = Uniform.Red;
             if (GUILayout.Button("Uninstall iOS 14 Advertising Support Package", GUILayout.MaxHeight(25f)))
