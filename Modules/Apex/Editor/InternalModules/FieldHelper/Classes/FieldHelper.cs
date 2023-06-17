e660e1659a84afb66fa240be1c5a4d59ffcab527
yenmoc <phongsoyenmoc.diep@gmail.com>
2023-06-17 18:08:44 +0700

    -update: performance

--- Modules/Apex/Editor/InternalModules/FieldHelper/Classes/FieldHelper.cs
+++ Modules/Apex/Editor/InternalModules/FieldHelper/Classes/FieldHelper.cs
@@ -25,80 +25,90 @@ namespace Pancake.ApexEditor
             InlineDecorators = new Dictionary<Type, FieldInlineDecorator>();
             Manipulators = new Dictionary<Type, MemberManipulator>();
 
-            Assembly[] assemblies = AppDomain.CurrentDomain.GetAssemblies();
-            for (int i = 0; i < assemblies.Length; i++)
+            TypeCache.TypeCollection drawerTargets = TypeCache.GetTypesWithAttribute<DrawerTarget>();
+            for (int i = 0; i < drawerTargets.Count; i++)
             {
-                Assembly assembly = assemblies[i];
-                Type[] types = assembly.GetTypes();
-                for (int j = 0; j < types.Length; j++)
+                Type type = drawerTargets[i];
+                if (Activator.CreateInstance(type) is FieldDrawer drawer)
                 {
-                    Type type = types[j];
-
-                    IEnumerable<DrawerTarget> drawerTargets = type.GetCustomAttributes<DrawerTarget>();
-                    if (drawerTargets != null)
+                    DrawerTarget attribute = type.GetCustomAttribute<DrawerTarget>();
+                    Drawers.Add(attribute.type, drawer);
+                    if (attribute.Subclasses)
                     {
-                        foreach (DrawerTarget drawerTarget in drawerTargets)
+                        foreach (Type subType in attribute.type.Subclasses())
                         {
-                            if (Activator.CreateInstance(type) is FieldDrawer drawer)
-                            {
-                                Drawers.Add(drawerTarget.type, drawer);
-                                if (drawerTarget.Subclasses)
-                                {
-                                    foreach (Type subType in drawerTarget.type.Subclasses())
-                                    {
-                                        Drawers.Add(subType, drawer);
-                                    }
-                                }
-                            }
-                        }
-                    }
-
-                    ViewTarget viewTarget = type.GetCustomAttribute<ViewTarget>();
-                    if (viewTarget != null)
-                    {
-                        if (Activator.CreateInstance(type) is FieldView view)
-                        {
-                            Views.Add(viewTarget.target, view);
-                        }
-                    }
-
-                    ValidatorTarget validatorTarget = type.GetCustomAttribute<ValidatorTarget>();
-                    if (validatorTarget != null)
-                    {
-                        if (Activator.CreateInstance(type) is FieldValidator validator)
-                        {
-                            Validators.Add(validatorTarget.target, validator);
-                        }
-                    }
-
-                    DecoratorTarget decoratorTarget = type.GetCustomAttribute<DecoratorTarget>();
-                    if (decoratorTarget != null)
-                    {
-                        if (Activator.CreateInstance(type) is FieldDecorator decorator)
-                        {
-                            Decorators.Add(decoratorTarget.target, decorator);
-                        }
-                    }
-
-                    InlineDecoratorTarget inlineDecoratorTarget = type.GetCustomAttribute<InlineDecoratorTarget>();
-                    if (inlineDecoratorTarget != null)
-                    {
-                        if (Activator.CreateInstance(type) is FieldInlineDecorator inlineDecorator)
-                        {
-                            InlineDecorators.Add(inlineDecoratorTarget.target, inlineDecorator);
-                        }
-                    }
-
-                    ManipulatorTarget ManipulatorTarget = type.GetCustomAttribute<ManipulatorTarget>();
-                    if (ManipulatorTarget != null)
-                    {
-                        if (Activator.CreateInstance(type) is MemberManipulator manipulator)
-                        {
-                            Manipulators.Add(ManipulatorTarget.target, manipulator);
+                            Drawers.Add(subType, drawer);
                         }
                     }
                 }
             }
+
+            Drawers.TrimExcess();
+
+            TypeCache.TypeCollection viewTargets = TypeCache.GetTypesWithAttribute<ViewTarget>();
+            for (int i = 0; i < viewTargets.Count; i++)
+            {
+                Type type = viewTargets[i];
+                if (Activator.CreateInstance(type) is FieldView view)
+                {
+                    ViewTarget attribute = type.GetCustomAttribute<ViewTarget>();
+                    Views.Add(attribute.target, view);
+                }
+            }
+
+            Views.TrimExcess();
+
+            TypeCache.TypeCollection validatorTargets = TypeCache.GetTypesWithAttribute<ValidatorTarget>();
+            for (int i = 0; i < validatorTargets.Count; i++)
+            {
+                Type type = validatorTargets[i];
+                if (Activator.CreateInstance(type) is FieldValidator validator)
+                {
+                    ValidatorTarget attribute = type.GetCustomAttribute<ValidatorTarget>();
+                    Validators.Add(attribute.target, validator);
+                }
+            }
+
+            Validators.TrimExcess();
+
+            TypeCache.TypeCollection decoratorTargets = TypeCache.GetTypesWithAttribute<DecoratorTarget>();
+            for (int i = 0; i < decoratorTargets.Count; i++)
+            {
+                Type type = decoratorTargets[i];
+                if (Activator.CreateInstance(type) is FieldDecorator decorator)
+                {
+                    DecoratorTarget attribute = type.GetCustomAttribute<DecoratorTarget>();
+                    Decorators.Add(attribute.target, decorator);
+                }
+            }
+
+            Decorators.TrimExcess();
+
+            TypeCache.TypeCollection inlineDecoratorTargets = TypeCache.GetTypesWithAttribute<InlineDecoratorTarget>();
+            for (int i = 0; i < inlineDecoratorTargets.Count; i++)
+            {
+                Type type = inlineDecoratorTargets[i];
+                if (Activator.CreateInstance(type) is FieldInlineDecorator inlineDecorator)
+                {
+                    InlineDecoratorTarget attribute = type.GetCustomAttribute<InlineDecoratorTarget>();
+                    InlineDecorators.Add(attribute.target, inlineDecorator);
+                }
+            }
+
+            InlineDecorators.TrimExcess();
+
+            TypeCache.TypeCollection manipulatorsTargets = TypeCache.GetTypesWithAttribute<ManipulatorTarget>();
+            for (int i = 0; i < manipulatorsTargets.Count; i++)
+            {
+                Type type = manipulatorsTargets[i];
+                if (Activator.CreateInstance(type) is MemberManipulator manipulator)
+                {
+                    ManipulatorTarget attribute = type.GetCustomAttribute<ManipulatorTarget>();
+                    Manipulators.Add(attribute.target, manipulator);
+                }
+            }
+
+            Manipulators.TrimExcess();
         }
     }
 }
