9d3a29d80af72651ff49dddf3a4d291feb2dc6b9
yenmoc <phongsoyenmoc.diep@gmail.com>
2023-06-21 10:19:51 +0700

    -add: bind input field

---
+++ Modules/Scriptable/Runtime/Bindings/BindInputField.cs
@@ -0,0 +1,23 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace Pancake.Scriptable
+{
+    [AddComponentMenu("Scriptable/Bindings/BindInputField")]
+    [RequireComponent(typeof(TMP_InputField))]
+    public class BindInputField : CacheGameComponent<TMP_InputField>
+    {
+        [SerializeField] private StringVariable stringVariable = null;
+
+        protected override void Awake()
+        {
+            base.Awake();
+            component.text = stringVariable;
+            component.onValueChanged.AddListener(SetBoundVariable);
+        }
+
+        private void OnDestroy() => component.onValueChanged.RemoveListener(SetBoundVariable);
+
+        private void SetBoundVariable(string value) => stringVariable.Value = value;
+    }
+}
