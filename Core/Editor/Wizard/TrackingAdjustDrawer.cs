ecfb382f87f370086ca24bac65774775be981598
yenmoc <phongsoyenmoc.diep@gmail.com>
2023-07-26 13:45:43 +0700

    -update: version adjust install

--- Core/Editor/Wizard/TrackingAdjustDrawer.cs
+++ Core/Editor/Wizard/TrackingAdjustDrawer.cs
@@ -12,7 +12,7 @@ namespace PancakeEditor
         public static void OnInspectorGUI()
         {
 #if PANCAKE_ADJUST
-            Uniform.DrawInstalled("4.33.0");
+            Uniform.DrawInstalled("4.33.2");
             EditorGUILayout.Space();
 
             var adjustSetting = Resources.Load<AdjustConfig>(nameof(AdjustConfig));
@@ -57,7 +57,7 @@ namespace PancakeEditor
             GUI.enabled = !EditorApplication.isCompiling;
             if (GUILayout.Button("Install Adjust Package", GUILayout.MaxHeight(40f)))
             {
-                RegistryManager.Add("com.pancake.adjust", "https://github.com/pancake-llc/adjust.git#4.33.0");
+                RegistryManager.Add("com.pancake.adjust", "https://github.com/pancake-llc/adjust.git#4.33.2");
                 RegistryManager.Resolve();
             }
 
