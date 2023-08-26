a9573a3ba067ce87298b61c5b0b44974e3a1bdc2
yenmoc <phongsoyenmoc.diep@gmail.com>
2023-08-26 13:02:27 +0700

    -update

--- Core/Editor/Wizard/UtilitiesParticleEffectForUGUIDrawer.cs
+++ Core/Editor/Wizard/UtilitiesParticleEffectForUGUIDrawer.cs
@@ -9,7 +9,7 @@ namespace PancakeEditor
         public static void OnInspectorGUI()
         {
 #if PANCAKE_PARTICLE_EFFECT_UGUI
-            Uniform.DrawInstalled("4.1.7");
+            Uniform.DrawInstalled("4.2.1");
             GUILayout.FlexibleSpace();
             GUI.backgroundColor = Uniform.Red;
             if (GUILayout.Button("Uninstall Particle Effect For UGUI", GUILayout.MaxHeight(25f)))
@@ -30,7 +30,7 @@ namespace PancakeEditor
             GUI.enabled = !EditorApplication.isCompiling;
             if (GUILayout.Button("Install Particle Effect For UGUI", GUILayout.MaxHeight(40f)))
             {
-                RegistryManager.Add("com.coffee.ui-particle", "https://github.com/mob-sakai/ParticleEffectForUGUI.git#4.1.7");
+                RegistryManager.Add("com.coffee.ui-particle", "https://github.com/mob-sakai/ParticleEffectForUGUI.git#4.2.1");
                 RegistryManager.Resolve();
             }
 
