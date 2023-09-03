1b4e4f8d5366dfc90e1445c550d293e6bdba575f
yenmoc <phongsoyenmoc.diep@gmail.com>
2023-09-03 16:55:31 +0700

    -refactor

--- Modules/LevelSystem/Runtime/ScriptableEventGetLevelCached.cs
+++ Modules/LevelSystem/Runtime/ScriptableEventGetLevelCached.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using Pancake.Scriptable;
+﻿using Pancake.Scriptable;
 using UnityEngine;
-using Object = UnityEngine.Object;
 
 namespace Pancake.LevelSystem
 {
