0cce667e5fa48e5182556465b982dc3d81074667
yenmoc <phongsoyenmoc.diep@gmail.com>
2023-09-12 16:28:10 +0700

    -add: scriptable event get game object

---
+++ Modules/Scriptable/Runtime/ScriptableEvents/ScriptableEventGetGameObject.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace Pancake.Scriptable
+{
+    [Searchable]
+    [EditorIcon("scriptable_event")]
+    [CreateAssetMenu(fileName = "event_get_gameobject.asset", menuName = "Pancake/Scriptable/Events/func gameobject")]
+    public class ScriptableEventGetGameObject : ScriptableEventFunc<GameObject>
+    {
+    }
+}
