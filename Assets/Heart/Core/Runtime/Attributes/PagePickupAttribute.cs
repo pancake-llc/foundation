72d9a371fd7934b7e8693cc4e7987458d2b6fd4f
yenmoc <phongsoyenmoc.diep@gmail.com>
2023-10-05 17:01:05 +0700

    -add: page pickup and sheet pickup

---
+++ Assets/Heart/Core/Runtime/Attributes/PagePickupAttribute.cs
@@ -0,0 +1,11 @@
+using Pancake.Apex;
+
+namespace Pancake
+{
+    /// <summary>
+    /// provides a way to select the name of the class from a collected list of names of all classes that inherit from T
+    /// </summary>
+    public class PagePickupAttribute : ViewAttribute
+    {
+    }
+}
