02fb1954d986f3cc1a9bf3797bf1d1891c0d72c9
yenmoc <phongsoyenmoc.diep@gmail.com>
2023-10-21 21:26:17 +0700

    -remove: circle element in rank element

--- Assets/_Root/Scripts/Popup/Leaderboard/LeaderboardElementView.cs
+++ Assets/_Root/Scripts/Popup/Leaderboard/LeaderboardElementView.cs
@@ -6,7 +6,7 @@ namespace Pancake.SceneFlow
 {
     using UnityEngine;
 
-    public class LeaderboardElementView : MonoBehaviour
+    public sealed class LeaderboardElementView : MonoBehaviour
     {
         [SerializeField] private TextMeshProUGUI txtRank;
         [SerializeField] private Image imgCountry;
@@ -17,11 +17,10 @@ namespace Pancake.SceneFlow
         [SerializeField] private Image imgOverlay2;
         [SerializeField] private GameObject you;
         [SerializeField] private Image imgHeader;
-        [SerializeField] private Image imgCircleRank;
         [SerializeField] private GameObject decor;
-        
-        
-        public virtual void Init(int rank, Sprite icon, string userName, int score, LeaderboardElementColor color, bool self)
+
+
+        public void Init(int rank, Sprite icon, string userName, int score, LeaderboardElementColor color, bool self)
         {
             txtRank.text = $"{rank}";
             txtUserName.text = userName;
@@ -37,21 +36,13 @@ namespace Pancake.SceneFlow
             you.SetActive(self);
             if (rank != 1 && rank != 2 && rank != 3)
             {
-                imgCircleRank.gameObject.SetActive(true);
                 txtRank.gameObject.SetActive(true);
                 decor.SetActive(false);
             }
             else
             {
-                imgCircleRank.gameObject.SetActive(false);
                 txtRank.gameObject.SetActive(false);
                 decor.SetActive(true);
-                // var eff = decor.GetComponentInChildren<ParticleSystemRenderer>();
-                // if (eff != null)
-                // {
-                //     eff.sortingLayerName = root.sortingLayerName;
-                //     eff.sortingOrder = root.sortingOrder + 1;
-                // }
             }
 
             TryGetComponent<UIShiny>(out var uiShiny);
