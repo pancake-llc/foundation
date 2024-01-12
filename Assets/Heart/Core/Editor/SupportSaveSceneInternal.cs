6d4b2027c410dedbcea50b1e6a75bb365fef99b5
yenmoc <phongsoyenmoc.diep@gmail.com>
2024-01-12 10:48:11 +0700

    feat: require ask scene save before enter play mode

---
+++ Assets/Heart/Core/Editor/SupportSaveSceneInternal.cs
@@ -0,0 +1,24 @@
+﻿using UnityEditor;
+using UnityEditor.SceneManagement;
+
+namespace PancakeEditor
+{
+    [InitializeOnLoad]
+    internal class SupportSaveSceneInternal
+    {
+        static SupportSaveSceneInternal() { EditorApplication.playModeStateChanged += OnPlayModeStateChanged; }
+
+        private static void OnPlayModeStateChanged(PlayModeStateChange state)
+        {
+            if (state == PlayModeStateChange.ExitingEditMode)
+            {
+                if (Internal.InternalData.Settings.requireSceneSave)
+                {
+                    if (EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo())
+                    {
+                    }
+                }
+            }
+        }
+    }
+}
