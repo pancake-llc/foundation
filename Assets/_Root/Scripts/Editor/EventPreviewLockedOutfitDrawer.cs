02e888d6f0a82d7979b905ee5cc30d9628b52d99
yenmoc <phongsoyenmoc.diep@gmail.com>
2024-01-15 09:51:01 +0700

    -fix: event preview locked outfit drawer

--- Assets/_Root/Scripts/Editor/EventPreviewLockedOutfitDrawer.cs
+++ Assets/_Root/Scripts/Editor/EventPreviewLockedOutfitDrawer.cs
@@ -1,8 +1,4 @@
 #if UNITY_EDITOR
-using System.Collections.Generic;
-using Pancake.ExLibEditor;
-using Pancake.Scriptable;
-using UnityEngine;
 using Pancake.SceneFlow;
 using UnityEditor;
 
@@ -11,26 +7,6 @@ namespace PancakeEditor
     [CustomEditor(typeof(ScriptableEventPreviewLockedOutfit))]
     public class EventPreviewLockedOutfitDrawer : UnityEditor.Editor
     {
-        public override void OnInspectorGUI()
-        {
-            DrawDefaultInspector();
-
-            Uniform.DrawLine();
-
-            var goContainer = (IDrawObjectsInInspector) target;
-            var gameObjects = goContainer.GetAllObjects();
-            if (gameObjects.Count > 0) DisplayAll(gameObjects);
-        }
-
-        private void DisplayAll(List<Object> objects)
-        {
-            GUILayout.Space(15);
-            var title = $"Listener Amount : {objects.Count}";
-
-            GUILayout.BeginVertical(title, "window");
-            foreach (var obj in objects) Uniform.DrawSelectableObject(obj, new[] {obj.name, "Select"});
-            GUILayout.EndVertical();
-        }
     }
 }
 #endif
