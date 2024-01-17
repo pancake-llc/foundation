f1d17cc3464530bb5c16f2362b7b990779895fdc
yenmoc <phongsoyenmoc.diep@gmail.com>
2024-01-17 12:00:43 +0700

    -update: draw advertising

--- Assets/Heart/Modules/Advertising/Editor/AdSettingsDrawer.cs
+++ Assets/Heart/Modules/Advertising/Editor/AdSettingsDrawer.cs
@@ -66,6 +66,11 @@ namespace Pancake.MonetizationEditor
             EditorGUILayout.PropertyField(_adCheckingIntervalProperty);
             EditorGUILayout.PropertyField(_adLoadingIntervalProperty);
             EditorGUILayout.PropertyField(_currentNetworkProperty);
+
+            GUILayout.Space(4);
+
+            if (_currentNetworkProperty.enumValueIndex == (int) EAdNetwork.Admob)
+            {
 #if PANCAKE_ADVERTISING && PANCAKE_ADMOB
             GUI.backgroundColor = Uniform.Green;
             EditorGUILayout.HelpBox("Admob plugin was imported", MessageType.Info);
@@ -111,23 +116,34 @@ namespace Pancake.MonetizationEditor
                 "ca-app-pub-3940256099942544~3347511713".CopyToClipboard();
                 DebugEditor.Toast("[Admob] Copy AppId Test Id Success!");
             }
+            
+            EditorGUILayout.PropertyField(_admobEnableTestModeProperty, new GUIContent("Test Mode"));
+            if (_admobEnableTestModeProperty.boolValue) EditorGUILayout.PropertyField(_admobDevicesTestProperty, new GUIContent("Devices Test"));
+
+            EditorGUILayout.PropertyField(_admobClientProperty, new GUIContent("Client"));
+            EditorGUILayout.PropertyField(_admobBannerProperty, new GUIContent("Banner"));
+            EditorGUILayout.PropertyField(_admobInterProperty, new GUIContent("Interstitial"));
+            EditorGUILayout.PropertyField(_admobRewardProperty, new GUIContent("Rewarded"));
+            EditorGUILayout.PropertyField(_admobRewardInterProperty, new GUIContent("Inter Rewarded"));
+            EditorGUILayout.PropertyField(_admobAppOpenProperty, new GUIContent("App Open"));
 #else
-            GUI.backgroundColor = Uniform.Orange;
-            EditorGUILayout.HelpBox("Admob plugin not found", MessageType.Warning);
-            GUI.backgroundColor = Color.white;
+                GUI.backgroundColor = Uniform.Orange;
+                EditorGUILayout.HelpBox("Admob plugin not found", MessageType.Warning);
+                GUI.backgroundColor = Color.white;
 
-            EditorGUILayout.BeginHorizontal();
-            GUI.backgroundColor = Uniform.Green;
-            if (GUILayout.Button("Install Admob SDK (1)", GUILayout.Height(24)))
-            {
-                DebugEditor.Log("<color=#FF77C6>[Ad]</color> importing admob sdk");
-                const string path = "Assets/Plugins/Android/GoogleMobileAdsPlugin.androidlib";
-                if (!Directory.Exists(path)) Directory.CreateDirectory(path);
-                string manifest = "<?xml version=\"1.0\" encoding=\"utf-8\"?>" +
-                                  "\n<manifest xmlns:android=\"http://schemas.android.com/apk/res/android\" package=\"com.google.unity.ads\" android:versionName=\"1.0\" android:versionCode=\"1\">" +
-                                  "\n<application><uses-library android:required=\"false\" android:name=\"org.apache.http.legacy\"/></application>" + "\n</manifest>";
+                EditorGUILayout.BeginHorizontal();
+                GUI.backgroundColor = Uniform.Green;
+                if (GUILayout.Button("Install Admob SDK v8.6.0 (1)", GUILayout.Height(24)))
+                {
+                    DebugEditor.Log("<color=#FF77C6>[Ad]</color> importing admob sdk");
+                    const string path = "Assets/Plugins/Android/GoogleMobileAdsPlugin.androidlib";
+                    if (!Directory.Exists(path)) Directory.CreateDirectory(path);
+                    const string manifest = "<?xml version=\"1.0\" encoding=\"utf-8\"?>" +
+                                            "\n<manifest xmlns:android=\"http://schemas.android.com/apk/res/android\" package=\"com.google.unity.ads\" android:versionName=\"1.0\" android:versionCode=\"1\">" +
+                                            "\n<application><uses-library android:required=\"false\" android:name=\"org.apache.http.legacy\"/></application>" +
+                                            "\n</manifest>";
 
-                string manifestMeta = @"fileFormatVersion: 2
+                    const string manifestMeta = @"fileFormatVersion: 2
 guid: e869b36b657604102a7166a39ee7d9c9
 labels:
 - gvh
@@ -141,22 +157,22 @@ TextScriptImporter:
   assetBundleVariant:
 ";
 
-                if (!File.Exists(path + "/AndroidManifest.xml"))
-                {
-                    var writer = new StreamWriter(path + "/AndroidManifest.xml", false);
-                    writer.Write(manifest);
-                    writer.Close();
-                }
+                    if (!File.Exists(path + "/AndroidManifest.xml"))
+                    {
+                        var writer = new StreamWriter(path + "/AndroidManifest.xml", false);
+                        writer.Write(manifest);
+                        writer.Close();
+                    }
 
-                if (!File.Exists(path + "/AndroidManifest.xml.meta"))
-                {
-                    var writer = new StreamWriter(path + "/AndroidManifest.xml.meta", false);
-                    writer.Write(manifestMeta);
-                    writer.Close();
-                }
+                    if (!File.Exists(path + "/AndroidManifest.xml.meta"))
+                    {
+                        var writer = new StreamWriter(path + "/AndroidManifest.xml.meta", false);
+                        writer.Write(manifestMeta);
+                        writer.Close();
+                    }
 
-                string properties = "target=android-31" + "\nandroid.library=true";
-                string propertiesMeta = @"fileFormatVersion: 2
+                    const string properties = "target=android-31" + "\nandroid.library=true";
+                    const string propertiesMeta = @"fileFormatVersion: 2
 guid: 1d06ef613dfec4baa88b56aeef69cd9c
 labels:
 - gvh
@@ -170,27 +186,27 @@ DefaultImporter:
   assetBundleVariant:
 ";
 
-                if (!File.Exists(path + "/project.properties"))
-                {
-                    var writer = new StreamWriter(path + "/project.properties", false);
-                    writer.Write(properties);
-                    writer.Close();
-                }
+                    if (!File.Exists(path + "/project.properties"))
+                    {
+                        var writer = new StreamWriter(path + "/project.properties", false);
+                        writer.Write(properties);
+                        writer.Close();
+                    }
 
-                if (!File.Exists(path + "/project.properties.meta"))
-                {
-                    var writer = new StreamWriter(path + "/project.properties.meta", false);
-                    writer.Write(propertiesMeta);
-                    writer.Close();
-                }
-                
-                string packagingOption = @"android {
+                    if (!File.Exists(path + "/project.properties.meta"))
+                    {
+                        var writer = new StreamWriter(path + "/project.properties.meta", false);
+                        writer.Write(propertiesMeta);
+                        writer.Close();
+                    }
+
+                    const string packagingOption = @"android {
     packagingOptions {
         pickFirst ""META-INF/kotlinx_coroutines_core.version""
     }
 }
 ";
-                string packagingOptionMeta = @"fileFormatVersion: 2
+                    const string packagingOptionMeta = @"fileFormatVersion: 2
 guid: 5b00d19726f74439b9a2fd9accf90795
 labels:
 - gvh
@@ -272,21 +288,21 @@ PluginImporter:
   assetBundleName:
   assetBundleVariant:
 ";
-                if (!File.Exists(path + "/packaging_options.gradle"))
-                {
-                    var writer = new StreamWriter(path + "/packaging_options.gradle", false);
-                    writer.Write(packagingOption);
-                    writer.Close();
-                }
+                    if (!File.Exists(path + "/packaging_options.gradle"))
+                    {
+                        var writer = new StreamWriter(path + "/packaging_options.gradle", false);
+                        writer.Write(packagingOption);
+                        writer.Close();
+                    }
 
-                if (!File.Exists(path + "/packaging_options.gradle.meta"))
-                {
-                    var writer = new StreamWriter(path + "/packaging_options.gradle.meta", false);
-                    writer.Write(packagingOptionMeta);
-                    writer.Close();
-                }
-                
-                string validateDependency = @"// Copyright (C) 2023 Google LLC
+                    if (!File.Exists(path + "/packaging_options.gradle.meta"))
+                    {
+                        var writer = new StreamWriter(path + "/packaging_options.gradle.meta", false);
+                        writer.Write(packagingOptionMeta);
+                        writer.Close();
+                    }
+
+                    const string validateDependency = @"// Copyright (C) 2023 Google LLC
 //
 // Licensed under the Apache License, Version 2.0 (the ""License"");
 // you may not use this file except in compliance with the License.
@@ -384,7 +400,7 @@ project(':unityLibrary:GoogleMobileAdsPlugin.androidlib') {
     }
 }
 ";
-                string validateDependencyMeta = @"fileFormatVersion: 2
+                    const string validateDependencyMeta = @"fileFormatVersion: 2
 guid: 9dfd887a15174d2b91b4e979131a7ac7
 labels:
 - gvh
@@ -466,53 +482,46 @@ PluginImporter:
   assetBundleName:
   assetBundleVariant:
 ";
-                
-                if (!File.Exists(path + "/validate_dependencies.gradle"))
-                {
-                    var writer = new StreamWriter(path + "/validate_dependencies.gradle", false);
-                    writer.Write(validateDependency);
-                    writer.Close();
+
+                    if (!File.Exists(path + "/validate_dependencies.gradle"))
+                    {
+                        var writer = new StreamWriter(path + "/validate_dependencies.gradle", false);
+                        writer.Write(validateDependency);
+                        writer.Close();
+                    }
+
+                    if (!File.Exists(path + "/validate_dependencies.gradle.meta"))
+                    {
+                        var writer = new StreamWriter(path + "/validate_dependencies.gradle.meta", false);
+                        writer.Write(validateDependencyMeta);
+                        writer.Close();
+                    }
+
+                    AssetDatabase.ImportAsset(path + "/AndroidManifest.xml");
+                    AssetDatabase.ImportAsset(path + "/AndroidManifest.xml.meta");
+                    AssetDatabase.ImportAsset(path + "/project.properties");
+                    AssetDatabase.ImportAsset(path + "/project.properties.meta");
+                    AssetDatabase.ImportPackage(ProjectDatabase.GetPathInCurrentEnvironent("Modules/Apex/ExLib/Core/Editor/Misc/UnityPackages/admob.unitypackage"),
+                        false);
                 }
 
-                if (!File.Exists(path + "/validate_dependencies.gradle.meta"))
+                GUI.backgroundColor = Uniform.Red;
+                if (GUILayout.Button("Add Admob Symbol (2)", GUILayout.Height(24)))
                 {
-                    var writer = new StreamWriter(path + "/validate_dependencies.gradle.meta", false);
-                    writer.Write(validateDependencyMeta);
-                    writer.Close();
+                    var group = BuildPipeline.GetBuildTargetGroup(EditorUserBuildSettings.activeBuildTarget);
+                    if (!ScriptingDefinition.IsSymbolDefined("PANCAKE_ADMOB", group))
+                    {
+                        ScriptingDefinition.AddDefineSymbolOnAllPlatforms("PANCAKE_ADMOB");
+                        AssetDatabase.Refresh();
+                    }
                 }
 
-                AssetDatabase.ImportAsset(path + "/AndroidManifest.xml");
-                AssetDatabase.ImportAsset(path + "/AndroidManifest.xml.meta");
-                AssetDatabase.ImportAsset(path + "/project.properties");
-                AssetDatabase.ImportAsset(path + "/project.properties.meta");
-                AssetDatabase.ImportPackage(ProjectDatabase.GetPathInCurrentEnvironent("Modules/Apex/ExLib/Core/Editor/Misc/UnityPackages/admob.unitypackage"), false);
-            }
-
-            GUI.backgroundColor = Uniform.Red;
-            if (GUILayout.Button("Add Admob Symbol (2)", GUILayout.Height(24)))
-            {
-                var group = BuildPipeline.GetBuildTargetGroup(EditorUserBuildSettings.activeBuildTarget);
-                if (!ScriptingDefinition.IsSymbolDefined("PANCAKE_ADMOB", group))
-                {
-                    ScriptingDefinition.AddDefineSymbolOnAllPlatforms("PANCAKE_ADMOB");
-                    AssetDatabase.Refresh();
-                }
-            }
-
-            GUI.backgroundColor = Color.white;
-            EditorGUILayout.EndHorizontal();
+                GUI.backgroundColor = Color.white;
+                EditorGUILayout.EndHorizontal();
 #endif
-            EditorGUILayout.PropertyField(_admobEnableTestModeProperty, new GUIContent("Test Mode"));
-            if (_admobEnableTestModeProperty.boolValue) EditorGUILayout.PropertyField(_admobDevicesTestProperty, new GUIContent("Devices Test"));
-
-            EditorGUILayout.PropertyField(_admobClientProperty, new GUIContent("Client"));
-            EditorGUILayout.PropertyField(_admobBannerProperty, new GUIContent("Banner"));
-            EditorGUILayout.PropertyField(_admobInterProperty, new GUIContent("Interstitial"));
-            EditorGUILayout.PropertyField(_admobRewardProperty, new GUIContent("Rewarded"));
-            EditorGUILayout.PropertyField(_admobRewardInterProperty, new GUIContent("Inter Rewarded"));
-            EditorGUILayout.PropertyField(_admobAppOpenProperty, new GUIContent("App Open"));
-
-            GUILayout.Space(20);
+            }
+            else if (_currentNetworkProperty.enumValueIndex == (int) EAdNetwork.Applovin)
+            {
 #if PANCAKE_ADVERTISING && PANCAKE_APPLOVIN
             GUI.backgroundColor = Uniform.Green;
             EditorGUILayout.HelpBox("Applovin plugin was imported", MessageType.Info);
@@ -540,34 +549,7 @@ PluginImporter:
 
             GUI.backgroundColor = Color.white;
             EditorGUILayout.EndHorizontal();
-#else
-            GUI.backgroundColor = Uniform.Orange;
-            EditorGUILayout.HelpBox("Applovin plugin not found", MessageType.Warning);
-            GUI.backgroundColor = Color.white;
-
-            EditorGUILayout.BeginHorizontal();
-            GUI.backgroundColor = Uniform.Green;
-            if (GUILayout.Button("Install AppLovin SDK (1)", GUILayout.Height(24)))
-            {
-                DebugEditor.Log("<color=#FF77C6>[Ad]</color> importing <color=#FF77C6>applovin</color> sdk");
-                AssetDatabase.ImportPackage(ProjectDatabase.GetPathInCurrentEnvironent("Modules/Apex/ExLib/Core/Editor/Misc/UnityPackages/applovin.unitypackage"), false);
-            }
-
-            GUI.backgroundColor = Uniform.Red;
-            if (GUILayout.Button("Add AppLovin Symbol (2)", GUILayout.Height(24)))
-            {
-                var group = BuildPipeline.GetBuildTargetGroup(EditorUserBuildSettings.activeBuildTarget);
-                if (!ScriptingDefinition.IsSymbolDefined("PANCAKE_APPLOVIN", group))
-                {
-                    ScriptingDefinition.AddDefineSymbolOnAllPlatforms("PANCAKE_APPLOVIN");
-                    AssetDatabase.Refresh();
-                }
-            }
-
-            GUI.backgroundColor = Color.white;
-            EditorGUILayout.EndHorizontal();
-
-#endif
+            
             EditorGUILayout.PropertyField(_sdkKeyPropertyProperty);
             EditorGUILayout.PropertyField(_applovinClientProperty, new GUIContent("Client"));
             EditorGUILayout.PropertyField(_applovinBannerProperty, new GUIContent("Banner"));
@@ -576,6 +558,37 @@ PluginImporter:
             EditorGUILayout.PropertyField(_applovinRewardInterProperty, new GUIContent("Inter Rewarded"));
             EditorGUILayout.PropertyField(_applovinAppOpenProperty, new GUIContent("App Open"));
             EditorGUILayout.PropertyField(_applovinEnableAgeRestrictedUserProperty, new GUIContent("Age Restricted"));
+#else
+                GUI.backgroundColor = Uniform.Orange;
+                EditorGUILayout.HelpBox("Applovin plugin not found", MessageType.Warning);
+                GUI.backgroundColor = Color.white;
+
+                EditorGUILayout.BeginHorizontal();
+                GUI.backgroundColor = Uniform.Green;
+                if (GUILayout.Button("Install AppLovin SDK (1)", GUILayout.Height(24)))
+                {
+                    DebugEditor.Log("<color=#FF77C6>[Ad]</color> importing <color=#FF77C6>applovin</color> sdk");
+                    AssetDatabase.ImportPackage(ProjectDatabase.GetPathInCurrentEnvironent("Modules/Apex/ExLib/Core/Editor/Misc/UnityPackages/applovin.unitypackage"),
+                        false);
+                }
+
+                GUI.backgroundColor = Uniform.Red;
+                if (GUILayout.Button("Add AppLovin Symbol (2)", GUILayout.Height(24)))
+                {
+                    var group = BuildPipeline.GetBuildTargetGroup(EditorUserBuildSettings.activeBuildTarget);
+                    if (!ScriptingDefinition.IsSymbolDefined("PANCAKE_APPLOVIN", group))
+                    {
+                        ScriptingDefinition.AddDefineSymbolOnAllPlatforms("PANCAKE_APPLOVIN");
+                        AssetDatabase.Refresh();
+                    }
+                }
+
+                GUI.backgroundColor = Color.white;
+                EditorGUILayout.EndHorizontal();
+
+#endif
+            }
+
 
             serializedObject.ApplyModifiedProperties();
         }
