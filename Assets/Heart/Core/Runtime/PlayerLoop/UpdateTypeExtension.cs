24c4baaf97651f2177f6891273204c467f3c6450
yenmoc <phongsoyenmoc.diep@gmail.com>
2024-01-17 09:57:00 +0700

    -refactor

--- Assets/Heart/Core/Runtime/PlayerLoop/UpdateTypeExtension.cs
+++ Assets/Heart/Core/Runtime/PlayerLoop/UpdateTypeExtension.cs
@@ -6,7 +6,7 @@ namespace Pancake.PlayerLoop
 {
     internal static class UpdateTypeExtension
     {
-        private static readonly Dictionary<UpdateType, Type> _updates = new()
+        private static readonly Dictionary<UpdateType, Type> Updates = new()
         {
             {UpdateType.EarlyUpdate, typeof(UnityPlayerLoop.EarlyUpdate)},
             {UpdateType.FixedUpdate, typeof(UnityPlayerLoop.FixedUpdate)},
@@ -16,8 +16,8 @@ namespace Pancake.PlayerLoop
             {UpdateType.PostLateUpdate, typeof(UnityPlayerLoop.PostLateUpdate)}
         };
 
-        public static Type ToType(this UpdateType plt) => _updates[plt];
-        public static int ToIndex(this UpdateType plt) => (int) plt;
+        public static Type ToType(this UpdateType updateType) => Updates[updateType];
+        public static int ToIndex(this UpdateType updateType) => (int) updateType;
         public static UpdateType FromIndex(int index) => (UpdateType) index;
     }
 }
