3537f8bf060bc32b5496a892addd2b41abe65b1f
yenmoc <phongsoyenmoc.diep@gmail.com>
2024-03-13 00:37:06 +0700

    -update: spine improve method extension

--- Assets/Heart/Modules/Spine/Spine.Extension.cs
+++ Assets/Heart/Modules/Spine/Spine.Extension.cs
@@ -23,6 +23,20 @@ namespace Pancake.Spine
             return animation.Duration;
         }
 
+        public static float Duration(this SkeletonAnimation skeleton, int track = 0)
+        {
+            var animation = skeleton.AnimationState.GetCurrent(track);
+            if (animation == null) return 0;
+            return animation.Animation.Duration;
+        }
+
+        public static float Duration(this SkeletonGraphic skeleton, int track = 0)
+        {
+            var animation = skeleton.AnimationState.GetCurrent(track);
+            if (animation == null) return 0;
+            return animation.Animation.Duration;
+        }
+
         public static SkeletonAnimation Play(this SkeletonAnimation skeleton, string animationName, bool loop = false)
         {
             skeleton.ClearState();
@@ -34,22 +48,62 @@ namespace Pancake.Spine
             return skeleton;
         }
 
-        public static SkeletonGraphic PlayOnly(this SkeletonGraphic skeleton, string animationName, bool loop = false)
+        public static SkeletonGraphic PlayOnly(this SkeletonGraphic skeleton, string animationName, bool loop = false, int trackIndex = 0)
         {
             skeleton.startingAnimation = animationName;
-            skeleton.AnimationState.SetAnimation(0, animationName, loop);
+            skeleton.AnimationState.SetAnimation(trackIndex, animationName, loop);
             return skeleton;
         }
 
-        public static SkeletonGraphic Play(this SkeletonGraphic skeleton, string animationName, bool loop = false)
+        public static SkeletonAnimation PlayOnly(this SkeletonAnimation skeleton, string animationName, bool loop = false, int trackIndex = 0)
+        {
+            skeleton.AnimationName = animationName;
+            skeleton.AnimationState.SetAnimation(trackIndex, animationName, loop);
+            return skeleton;
+        }
+
+        public static SkeletonGraphic Play(this SkeletonGraphic skeleton, string animationName, bool loop = false, int trackIndex = 0)
         {
             skeleton.Clear();
             skeleton.startingAnimation = animationName;
             skeleton.startingLoop = loop;
-            skeleton.AnimationState.SetAnimation(0, animationName, loop);
+            skeleton.AnimationState.SetAnimation(trackIndex, animationName, loop);
             skeleton.LateUpdate();
             skeleton.Initialize(true);
 
+            return skeleton;
+        }
+
+        public static SkeletonAnimation PlayWithNextAnimation(
+            this SkeletonAnimation skeleton,
+            string animationName,
+            string nextAnimation,
+            bool loop = false,
+            int trackIndex = 0,
+            float delay = 0)
+        {
+            skeleton.AnimationName = animationName;
+            skeleton.AnimationState.SetAnimation(trackIndex, animationName, loop);
+            skeleton.AddAnimation(nextAnimation, loop, trackIndex, delay);
+            return skeleton;
+        }
+
+        public static SkeletonAnimation AddAnimation(
+            this SkeletonAnimation skeleton,
+            string animationName,
+            bool loop = false,
+            int trackIndex = 0,
+            float delay = 0,
+            float mixDuration = 0f)
+        {
+            var trackEntry = skeleton.AnimationState.AddAnimation(trackIndex, animationName, loop, delay);
+            trackEntry.MixDuration = mixDuration;
+            return skeleton;
+        }
+
+        public static SkeletonAnimation AddEmptyAnimation(this SkeletonAnimation skeleton, int trackIndex = 0, float mixDuration = 0f, float delay = 0f)
+        {
+            skeleton.AnimationState.AddEmptyAnimation(trackIndex, mixDuration, delay);
             return skeleton;
         }
 
@@ -112,27 +166,27 @@ namespace Pancake.Spine
         }
 
 
-        public static SkeletonAnimation OnComplete(this SkeletonAnimation skeleton, Action onComplete, MonoBehaviour target = null)
+        public static SkeletonAnimation OnComplete(this SkeletonAnimation skeleton, Action onComplete, int track = 0, MonoBehaviour target = null)
         {
-            App.Delay(target, skeleton.Duration(skeleton.AnimationName), onComplete);
+            App.Delay(target, skeleton.Duration(track), onComplete);
             return skeleton;
         }
 
-        public static SkeletonGraphic OnComplete(this SkeletonGraphic skeleton, Action onComplete, MonoBehaviour target = null)
+        public static SkeletonGraphic OnComplete(this SkeletonGraphic skeleton, Action onComplete, int track = 0, MonoBehaviour target = null)
         {
-            App.Delay(target, skeleton.Duration(skeleton.startingAnimation), onComplete);
+            App.Delay(target, skeleton.Duration(track), onComplete);
             return skeleton;
         }
 
-        public static SkeletonAnimation OnUpdate(this SkeletonAnimation skeleton, Action<float> onUpdate, MonoBehaviour target = null)
+        public static SkeletonAnimation OnUpdate(this SkeletonAnimation skeleton, Action<float> onUpdate, int track = 0, MonoBehaviour target = null)
         {
-            App.Delay(target, skeleton.Duration(skeleton.AnimationName), null, onUpdate);
+            App.Delay(target, skeleton.Duration(track), null, onUpdate);
             return skeleton;
         }
 
-        public static SkeletonGraphic OnUpdate(this SkeletonGraphic skeleton, Action<float> onUpdate, MonoBehaviour target = null)
+        public static SkeletonGraphic OnUpdate(this SkeletonGraphic skeleton, Action<float> onUpdate, int track = 0, MonoBehaviour target = null)
         {
-            App.Delay(target, skeleton.Duration(skeleton.startingAnimation), null, onUpdate);
+            App.Delay(target, skeleton.Duration(track), null, onUpdate);
             return skeleton;
         }
 
