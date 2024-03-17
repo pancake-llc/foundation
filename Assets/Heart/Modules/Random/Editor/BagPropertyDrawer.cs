40352a316af524181a133c4eb0b7ae48ef60a43a
yenmoc <phongsoyenmoc.diep@gmail.com>
2024-03-17 17:46:21 +0700

    -update: bag

---
+++ Assets/Heart/Modules/Random/Editor/BagPropertyDrawer.cs
@@ -0,0 +1,111 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Pancake.Editor
+{
+    [CustomPropertyDrawer(typeof(Bag<>), true)]
+    public class BagPropertyDrawer : PropertyDrawer
+    {
+        private SerializedProperty _data;
+        public override float GetPropertyHeight(SerializedProperty property, GUIContent label) { return EditorGUI.GetPropertyHeight(property, label, true); }
+
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            EditorGUI.BeginProperty(position, label, property);
+            EditorGUI.PropertyField(position, property, label, true);
+
+            _data = property.FindPropertyRelative("data");
+
+            if (property.isExpanded && _data.isExpanded)
+            {
+                var rect = new Rect(position.x + 150, position.y + 44.6f, position.width, EditorGUIUtility.singleLineHeight);
+                float minProbability = GetMinProbability();
+                float maxProbability = GetMaxProbability();
+
+                for (int i = 0; i < _data.arraySize; i++)
+                {
+                    var element = _data.GetArrayElementAtIndex(i);
+
+                    EditorGUI.LabelField(rect, new GUIContent("Probability : "));
+
+                    float probability = GetProbabilityFor(i);
+                    var style = new GUIStyle {normal = {textColor = Color.red}};
+
+                    if (probability > 0)
+                    {
+                        style.normal.textColor = Color.cyan;
+                        if (Mathf.Approximately(maxProbability, probability))
+                        {
+                            style.normal.textColor = Color.green;
+                        }
+                        else if (Mathf.Approximately(minProbability, probability))
+                        {
+                            style.normal.textColor = new Color(1f, 0.6f, 0.04f, 1f);
+                        }
+                    }
+                    else if (Mathf.Approximately(probability, 0f))
+                    {
+                        style.normal.textColor = Color.yellow;
+                    }
+
+                    EditorGUI.LabelField(new Rect(rect.x + 70, rect.y + 2, rect.width, rect.height), new GUIContent(probability + "%"), style);
+
+                    rect.y += GetPropertyHeight(element, label) + 2f;
+                }
+            }
+
+            EditorGUI.EndProperty();
+            return;
+
+            float GetMinProbability()
+            {
+                var minValue = float.MaxValue;
+                for (var i = 0; i < _data.arraySize; i++)
+                {
+                    float probability = GetProbabilityFor(i);
+                    if (minValue > probability)
+                    {
+                        minValue = probability;
+                    }
+                }
+
+                return minValue;
+            }
+
+            float GetMaxProbability()
+            {
+                var maxValue = 0f;
+                for (var i = 0; i < _data.arraySize; i++)
+                {
+                    float probability = GetProbabilityFor(i);
+                    if (maxValue < probability)
+                    {
+                        maxValue = probability;
+                    }
+                }
+
+                return maxValue;
+            }
+
+            float GetProbabilityFor(int index)
+            {
+                var value = 1f;
+                var total = 0f;
+                
+                SerializedProperty element;
+                for (var i = 0; i < _data.arraySize; i++)
+                {
+                    element = _data.GetArrayElementAtIndex(i);
+                    element = element.FindPropertyRelative("weight");
+                    total += element.floatValue;
+                }
+
+                element = _data.GetArrayElementAtIndex(index);
+                element = element.FindPropertyRelative("weight");
+                value = element.floatValue;
+
+                return value / total * 100f;
+            }
+        }
+    }
+}
---
+++ Assets/Heart/Modules/Random/Runtime/Bag.cs
@@ -0,0 +1,118 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace Pancake
+{
+    [Serializable]
+    public class BagItem<T>
+    {
+        public T item;
+        [Min(0f)] public float weight;
+    }
+
+    [Serializable]
+    public class Bag<T>
+    {
+        [SerializeField] private List<BagItem<T>> data;
+        private float[] _cumulativeWeights;
+        private float _totalWeight;
+
+        public Bag(params BagItem<T>[] items)
+        {
+            data = new List<BagItem<T>>(items);
+            Initialize();
+        }
+
+        public void Initialize()
+        {
+            _cumulativeWeights = new float[data.Count];
+            UpdateCumulativeWeights();
+        }
+
+        public T Pick()
+        {
+            if (data == null || data.Count == 0) return default;
+
+            float point = Random.Range(0f, _totalWeight);
+            var lo = 0;
+            int hi = _cumulativeWeights.Length - 1;
+
+            while (lo < hi)
+            {
+                var mid = (int) (((uint) hi + (uint) lo) >> 1);
+                float value = _cumulativeWeights[mid];
+
+                if (point > value)
+                {
+                    lo = mid + 1;
+                }
+                else
+                {
+                    float prevPoint = mid > 0 ? _cumulativeWeights[mid - 1] : 0;
+
+                    if (point >= prevPoint) return data[mid].item;
+
+                    hi = mid;
+                }
+            }
+
+            return data[hi].item;
+        }
+
+        public T Pop()
+        {
+            if (data == null || data.Count == 0) return default;
+            
+            float point = Random.Range(0f, _totalWeight);
+            var lo = 0;
+            int hi = _cumulativeWeights.Length - 1;
+
+            while (lo < hi)
+            {
+                var mid = (int) (((uint) hi + (uint) lo) >> 1);
+                float value = _cumulativeWeights[mid];
+
+                if (point > value)
+                {
+                    lo = mid + 1;
+                }
+                else
+                {
+                    float prevPoint = mid > 0 ? _cumulativeWeights[mid - 1] : 0;
+
+                    if (point >= prevPoint)
+                    {
+                        var item = data[mid];
+                        data.RemoveAt(mid);
+                        _totalWeight -= item.weight;
+                        UpdateCumulativeWeights();
+                        return item.item;
+                    }
+
+                    hi = mid;
+                }
+            }
+
+            int l = _cumulativeWeights.Length - 1;
+            var result = data[l];
+            data.RemoveAt(l);
+            _totalWeight -= result.weight;
+            UpdateCumulativeWeights();
+            return result.item;
+        }
+
+        private void UpdateCumulativeWeights()
+        {
+            float totalWeight = 0;
+            for (var i = 0; i < data.Count; i++)
+            {
+                totalWeight += data[i].weight;
+                _cumulativeWeights[i] = totalWeight;
+            }
+
+            _totalWeight = totalWeight;
+        }
+    }
+}
