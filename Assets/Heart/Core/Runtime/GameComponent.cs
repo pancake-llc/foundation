a714fb2c5b4643bf19ea8fc56e7dea07c0a4cf92
yenmoc <phongsoyenmoc.diep@gmail.com>
2024-05-05 22:08:57 +0700

    -add: load component as a part of GameComponent

--- Assets/Heart/Core/Runtime/GameComponent.cs
+++ Assets/Heart/Core/Runtime/GameComponent.cs
@@ -6,5 +6,17 @@ namespace Pancake
     {
         public UnityEngine.GameObject GameObject => gameObject;
         public UnityEngine.Transform Transform => transform;
+
+        protected virtual void OnLoadComponents() { }
+
+#if UNITY_EDITOR
+        [Alchemy.Inspector.HorizontalLine(0.87f, 0.44f, 0.64f)]
+        [Alchemy.Inspector.Button]
+        private void LoadComponents()
+        {
+            OnLoadComponents();
+            UnityEditor.EditorUtility.SetDirty(this);
+        }
+#endif
     }
 }
