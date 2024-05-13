d936dfaa2980350c13359c7fe03b0d117190ae17
yenmoc <phongsoyenmoc.diep@gmail.com>
2024-05-13 23:28:31 +0700

    -remove

--- Assets/Heart/Modules/Raycast/AsyncRaycast.cs
+++
@@ -1,9 +0,0 @@
-namespace Pancake.Physics
-{
-    public struct AsyncRaycast
-    {
-        public RaycastTicket ticket;
-        public object context;
-        public System.Action<AsyncRaycastCallbackResult> onComplete;
-    }
-}
