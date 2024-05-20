757b90847f3d360c1f50e002a4694a0fec91b5db
yenmoc <phongsoyenmoc.diep@gmail.com>
2024-05-20 23:21:05 +0700

    -normalize

--- Assets/_Root/Scripts/Initialization/LevelInitialization.cs
+++ Assets/_Root/Scripts/Initialization/LevelInitialization.cs
@@ -14,7 +14,7 @@ namespace Pancake.SceneFlow
         public override async void Init()
         {
             await LevelCoordinator.LoadLevel(levelType.Value, LevelCoordinator.GetCurrentLevelIndex(levelType.Value));
-            Router.Default.PublishAsync(new LevelLoadedNoticeCommand());
+            await Router.Default.PublishAsync(new LevelLoadedNoticeCommand());
         }
     }
 }
