b3951b982707c002b3f80511c19ab1720f604519
yenmoc <phongsoyenmoc.diep@gmail.com>
2024-06-20 23:42:32 +0700

    -add: sound attribute

---
+++ Assets/Heart/Modules/Sound/new/Attributes/FrequencyAttribute.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using UnityEngine;
+
+namespace Pancake.Sound
+{
+    [AttributeUsage(AttributeTargets.Field)]
+    public class FrequencyAttribute : PropertyAttribute
+    {
+    }
+}
