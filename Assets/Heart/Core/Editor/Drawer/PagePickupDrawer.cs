3f27b4cd0c470214498b3ed6082024f7a45104f2
yenmoc <phongsoyenmoc.diep@gmail.com>
2024-07-07 23:23:23 +0700

    -add: bring back pickup

---
+++ Assets/Heart/Core/Editor/Drawer/PagePickupDrawer.cs
@@ -0,0 +1,106 @@
+﻿using Alchemy.Editor;
+using Pancake;
+using Pancake.Linq;
+using Pancake.UI;
+using PancakeEditor.Common;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace PancakeEditor
+{
+    [CustomAttributeDrawer(typeof(PagePickupAttribute))]
+    public class PagePickupDrawer : AlchemyAttributeDrawer
+    {
+        public override void OnCreateElement()
+        {
+            // Create a container for the horizontal layout
+            var container = new VisualElement {style = {flexDirection = FlexDirection.Row, alignItems = Align.Center, marginBottom = 4}};
+
+            var label = new Label(ObjectNames.NicifyVariableName(SerializedProperty.name.ToCamelCase())) {style = {marginLeft = 3, marginRight = 10, flexGrow = 0}};
+
+            var button = new Button
+            {
+                text = "Select type...",
+                style =
+                {
+                    flexGrow = 1,
+                    marginLeft = 10,
+                    flexShrink = 0,
+                    marginRight = -3,
+                    backgroundColor = new Color(0f, 0.18f, 0.53f, 0.31f),
+                    color = Color.white
+                }
+            };
+
+            if (!string.IsNullOrEmpty(SerializedProperty.stringValue))
+            {
+                var type = GetTypeByFullName();
+                if (type != null)
+                {
+                    var result = type.GetAllSubClass<Popup>();
+                    foreach (var t in result)
+                    {
+                        if (t.Namespace == SerializedProperty.stringValue)
+                        {
+                            button.text = SerializedProperty.stringValue;
+                            break;
+                        }
+
+                        button.text = "Failed load...";
+                    }
+                }
+            }
+
+            button.clicked += () =>
+            {
+                var menu = new GenericMenu();
+                menu.AddItem(new GUIContent("None (-1)"),
+                    false,
+                    () =>
+                    {
+                        SetAndApplyProperty(SerializedProperty, string.Empty);
+                        button.text = "Select type...";
+                    });
+                var type = GetTypeByFullName();
+                if (type != null)
+                {
+                    var result = type.GetAllSubClass<Popup>().Filter(t => !t.Name.Equals("Page`1"));
+                    for (var i = 0; i < result.Count; i++)
+                    {
+                        if (i == 0) menu.AddSeparator("");
+                        int cachei = i;
+                        menu.AddItem(new GUIContent($"{result[cachei].Name} ({cachei})"),
+                            false,
+                            () =>
+                            {
+                                SetAndApplyProperty(SerializedProperty, result[cachei].Name);
+                                button.text = result[cachei].Name;
+                            });
+                    }
+                }
+
+                menu.DropDown(new Rect(button.worldBound.position, Vector2.zero));
+            };
+
+
+            container.Add(label);
+            container.Add(button);
+
+            TargetElement.Clear();
+            TargetElement.Add(container);
+        }
+
+        private static System.Type GetTypeByFullName()
+        {
+            TypeExtensions.TryFindTypeByFullName("Pancake.UI.Page", out var type);
+            return type;
+        }
+
+        private void SetAndApplyProperty(SerializedProperty property, string value)
+        {
+            property.stringValue = value;
+            property.serializedObject.ApplyModifiedProperties();
+        }
+    }
+}
--- Assets/Heart/Modules/Sound/Editor/AudioIdPickupDrawer.cs
+++ Assets/Heart/Modules/Sound/Editor/AudioIdPickupDrawer.cs
@@ -5,94 +5,93 @@ using UnityEditor;
 using UnityEngine;
 using UnityEngine.UIElements;
 
-[CustomAttributeDrawer(typeof(AudioPickupAttribute))]
-public class AudioIdPickupDrawer : AlchemyAttributeDrawer
+namespace PancakeEditor.Sound
 {
-    public override void OnCreateElement()
+    [CustomAttributeDrawer(typeof(AudioPickupAttribute))]
+    public class AudioIdPickupDrawer : AlchemyAttributeDrawer
     {
-        var idProperty = SerializedProperty.FindPropertyRelative("id");
-        var nameProperty = SerializedProperty.FindPropertyRelative("name");
-
-        // Create a container for the horizontal layout
-        var container = new VisualElement {style = {flexDirection = FlexDirection.Row, alignItems = Align.Center}};
-
-        var label = new Label(SerializedProperty.name) {style = {marginRight = 25}};
-
-        var textField = new TextField
+        public override void OnCreateElement()
         {
-            value = nameProperty.stringValue,
-            isReadOnly = true,
-            focusable = false,
-            style =
-            {
-                flexGrow = 1, // Make the text field expand to fill the available space
-                marginRight = 5 // Add some margin to the right for spacing
-            }
-        };
-        textField.SetEnabled(false);
+            if (SerializedProperty.type != nameof(AudioId)) return;
+            var idProperty = SerializedProperty.FindPropertyRelative("id");
+            var nameProperty = SerializedProperty.FindPropertyRelative("name");
 
-        var button = new Button {text = "Select type..."};
+            // Create a container for the horizontal layout
+            var container = new VisualElement {style = {flexDirection = FlexDirection.Row, alignItems = Align.Center, marginBottom = 4}};
 
-        // Set the initial text of the button
-        if (!string.IsNullOrEmpty(idProperty.stringValue))
-        {
-            var allAudioAsset = ProjectDatabase.FindAll<AudioData>();
-            foreach (var t in allAudioAsset)
+            var label = new Label(ObjectNames.NicifyVariableName(SerializedProperty.name.ToCamelCase())) {style = {marginLeft = 3, marginRight = 10, flexGrow = 0}};
+
+            var button = new Button
             {
-                if (t.id == idProperty.stringValue)
+                text = "Select type...",
+                style =
                 {
-                    button.text = nameProperty.stringValue;
-                    textField.value = nameProperty.stringValue;
-                    break;
+                    flexGrow = 1,
+                    marginLeft = 10,
+                    flexShrink = 0,
+                    marginRight = -3,
+                    backgroundColor = new Color(0.99f, 0.5f, 0.24f, 0.31f),
+                    color = Color.white
                 }
+            };
 
-                button.text = "Failed load...";
-            }
-        }
-
-        button.clicked += () =>
-        {
-            var menu = new GenericMenu();
-            var allAudioAsset = ProjectDatabase.FindAll<AudioData>();
-            menu.AddItem(new GUIContent("None (-1)"),
-                false,
-                () =>
-                {
-                    SetAndApplyProperty(idProperty, string.Empty);
-                    textField.value = string.Empty;
-                    button.text = "Select type...";
-                });
-            for (var i = 0; i < allAudioAsset.Count; i++)
+            // Set the initial text of the button
+            if (!string.IsNullOrEmpty(idProperty.stringValue))
             {
-                if (i == 0) menu.AddSeparator("");
-                int cachei = i;
-                menu.AddItem(new GUIContent($"{allAudioAsset[cachei].name} ({cachei})"),
+                var allAudioAsset = ProjectDatabase.FindAll<AudioData>();
+                foreach (var t in allAudioAsset)
+                {
+                    if (t.id == idProperty.stringValue)
+                    {
+                        button.text = nameProperty.stringValue;
+                        break;
+                    }
+
+                    button.text = "Failed load...";
+                }
+            }
+
+            button.clicked += () =>
+            {
+                var menu = new GenericMenu();
+                var allAudioAsset = ProjectDatabase.FindAll<AudioData>();
+                menu.AddItem(new GUIContent("None (-1)"),
                     false,
                     () =>
                     {
-                        SetAndApplyProperty(idProperty, allAudioAsset[cachei].id);
-                        SetAndApplyProperty(nameProperty, allAudioAsset[cachei].name);
-                        textField.value = allAudioAsset[cachei].name;
-                        button.text = allAudioAsset[cachei].name;
+                        SetAndApplyProperty(idProperty, string.Empty);
+                        button.text = "Select type...";
                     });
-            }
+                for (var i = 0; i < allAudioAsset.Count; i++)
+                {
+                    if (i == 0) menu.AddSeparator("");
+                    int cachei = i;
+                    menu.AddItem(new GUIContent($"{allAudioAsset[cachei].name} ({cachei})"),
+                        false,
+                        () =>
+                        {
+                            SetAndApplyProperty(idProperty, allAudioAsset[cachei].id);
+                            SetAndApplyProperty(nameProperty, allAudioAsset[cachei].name);
+                            button.text = allAudioAsset[cachei].name;
+                        });
+                }
 
-            menu.DropDown(new Rect(button.worldBound.position, Vector2.zero));
-        };
+                menu.DropDown(new Rect(button.worldBound.position, Vector2.zero));
+            };
 
 
-        container.Add(label);
-        container.Add(textField);
-        container.Add(button);
+            container.Add(label);
+            container.Add(button);
 
-        // Add the container to the target element, which will hide default UI
-        TargetElement.Clear();
-        TargetElement.Add(container);
-    }
+            // Add the container to the target element, which will hide default UI
+            TargetElement.Clear();
+            TargetElement.Add(container);
+        }
 
-    private void SetAndApplyProperty(SerializedProperty property, string value)
-    {
-        property.stringValue = value;
-        property.serializedObject.ApplyModifiedProperties();
+        private void SetAndApplyProperty(SerializedProperty property, string value)
+        {
+            property.stringValue = value;
+            property.serializedObject.ApplyModifiedProperties();
+        }
     }
 }
