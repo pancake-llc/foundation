a620610c528b51e29f0cdb94054dd040795d13c8
yenmoc <phongsoyenmoc.diep@gmail.com>
2024-07-07 23:52:35 +0700

    -update: add cancellation

--- Assets/Heart/Modules/Pool/UniTask/AsyncObjectPool.cs
+++ Assets/Heart/Modules/Pool/UniTask/AsyncObjectPool.cs
@@ -9,18 +9,26 @@ namespace Pancake.Pools
     {
         public AsyncObjectPool(Func<UniTask<T>> createFunc, Action<T> onRequest = null, Action<T> onReturn = null, Action<T> onDestroy = null)
         {
+            _createFunc = _ => createFunc();
+            _onRequest = onRequest;
+            _onReturn = onReturn;
+            _onDestroy = onDestroy;
+        }
+
+        public AsyncObjectPool(Func<CancellationToken, UniTask<T>> createFunc, Action<T> onRequest = null, Action<T> onReturn = null, Action<T> onDestroy = null)
+        {
             _createFunc = createFunc ?? throw new ArgumentException(nameof(createFunc));
             _onRequest = onRequest;
             _onReturn = onReturn;
             _onDestroy = onDestroy;
         }
 
-        private readonly Func<UniTask<T>> _createFunc;
+        private readonly Func<CancellationToken, UniTask<T>> _createFunc;
         private readonly Action<T> _onRequest;
         private readonly Action<T> _onReturn;
         private readonly Action<T> _onDestroy;
 
-        protected override UniTask<T> CreateInstanceAsync(CancellationToken cancellationToken) { return _createFunc(); }
+        protected override UniTask<T> CreateInstanceAsync(CancellationToken cancellationToken) { return _createFunc(cancellationToken); }
 
         protected override void OnDestroy(T instance) { _onDestroy?.Invoke(instance); }
 
