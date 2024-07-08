8dfba4534b2b59a2f67581118ab1e4748629bb55
yenmoc <phongsoyenmoc.diep@gmail.com>
2024-07-08 23:54:30 +0700

    -add: custom editor info

---
+++ Assets/Heart/Common/Editor/ComponentHeaderWrapper.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace PancakeEditor
+{
+    /// <summary>
+    /// Class that can wrap the <see cref="IMGUIContainer.onGUIHandler"/>
+    /// of a component header <see cref="IMGUIContainer"/> and allow injecting
+    /// drawing logic to occur before and after its drawing.
+    /// </summary>
+    internal class ComponentHeaderWrapper
+    {
+        private readonly IMGUIContainer headerElement;
+        private readonly Component component;
+        private readonly Action wrappedOnGUIHandler;
+        private readonly bool supportsRichText;
+
+        private ComponentHeaderWrapper(IMGUIContainer headerElement, Component component, bool supportsRichText)
+        {
+            this.headerElement = headerElement;
+            this.component = component;
+            this.supportsRichText = supportsRichText;
+            wrappedOnGUIHandler = headerElement.onGUIHandler;
+        }
+
+        public static void WrapIfNotAlreadyWrapped((Editor editor, IMGUIContainer header) editorAndHeader, bool supportsRichText)
+        {
+            var header = editorAndHeader.header;
+            var onGUIHandler = header.onGUIHandler;
+            if (string.Equals(onGUIHandler.Method?.Name, nameof(DrawWrappedHeaderGUI)))
+            {
+                return;
+            }
+
+            var targetComponent = editorAndHeader.editor.target as Component;
+            var wrapper = new ComponentHeaderWrapper(header, targetComponent, supportsRichText);
+            header.onGUIHandler = wrapper.DrawWrappedHeaderGUI;
+        }
+
+        private void DrawWrappedHeaderGUI()
+        {
+            if (component == null)
+            {
+                Unwrap();
+                return;
+            }
+
+            Rect headerRect = headerElement.contentRect;
+            bool HeaderIsSelected = headerElement.focusController.focusedElement == headerElement;
+
+            ComponentHeader.InvokeBeforeHeaderGUI(component, headerRect, HeaderIsSelected, supportsRichText);
+            wrappedOnGUIHandler?.Invoke();
+            ComponentHeader.InvokeAfterHeaderGUI(component, headerRect, HeaderIsSelected, supportsRichText);
+        }
+
+        private void Unwrap()
+        {
+            if (headerElement is not null)
+            {
+                headerElement.onGUIHandler = wrappedOnGUIHandler;
+            }
+        }
+    }
+}
---
+++ Assets/Heart/Common/Editor/ComponentHeaderWrapperToInspectorInjector.cs
@@ -0,0 +1,50 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UIElements;
+using static PancakeEditor.InspectorContents;
+
+namespace PancakeEditor
+{
+    [InitializeOnLoad]
+    internal static class ComponentHeaderWrapperToInspectorInjector
+    {
+        static ComponentHeaderWrapperToInspectorInjector()
+        {
+            Editor.finishedDefaultHeaderGUI -= AfterInspectorRootEditorHeaderGUI;
+            Editor.finishedDefaultHeaderGUI += AfterInspectorRootEditorHeaderGUI;
+        }
+
+        private static void AfterInspectorRootEditorHeaderGUI(Editor editor)
+        {
+            if (editor.target is GameObject)
+            {
+                // Handle InspectorWindow
+                AfterGameObjectHeaderGUI(editor);
+                return;
+            }
+
+            if (editor.target is Component)
+            {
+                // Handle PropertyEditor window opened via "Properties..." context menu item
+                AfterComponentPropertiesHeaderGUI(editor);
+            }
+        }
+
+        private static void AfterGameObjectHeaderGUI([DisallowNull] Editor gameObjectEditor)
+        {
+            foreach (var editorAndHeader in GetComponentHeaderElementsFromEditorWindowOf(gameObjectEditor))
+            {
+                ComponentHeaderWrapper.WrapIfNotAlreadyWrapped(editorAndHeader, true);
+            }
+        }
+
+        private static void AfterComponentPropertiesHeaderGUI([DisallowNull] Editor componentEditor)
+        {
+            if (GetComponentHeaderElementFromPropertyEditorOf(componentEditor) is (Editor, IMGUIContainer) editorAndHeader)
+            {
+                ComponentHeaderWrapper.WrapIfNotAlreadyWrapped(editorAndHeader, false);
+            }
+        }
+    }
+}
