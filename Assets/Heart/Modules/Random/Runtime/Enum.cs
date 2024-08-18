de5f67905d9feb95634d235a80d1a73375caca5a
aprius <phongsoyenmoc.diep@gmail.com>
2024-08-18 13:01:15 +0700

    -add: Add algorithm to make randomization in Bag more unpredictable and adjustable as desired

---
+++ Assets/Heart/Modules/Random/Runtime/Enum.cs
@@ -0,0 +1,17 @@
+﻿namespace Pancake
+{
+    public enum ENumberRoll
+    {
+        One,
+        Two,
+        Three
+    }
+
+    public enum ERollResultType
+    {
+        None,
+        Min,
+        Max,
+        Sum
+    }
+}
