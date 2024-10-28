42efb7ed642178b7958a68ae47c76483a88bebc0
aprius <phongsoyenmoc.diep@gmail.com>
2024-10-28 22:54:21 +0700

    -add: decorator pattern

---
+++ Assets/Heart/Core/Runtime/Patterns/Structural/Decorator/Decorable.cs
@@ -0,0 +1,52 @@
+﻿namespace Pancake.Pattern
+{
+    /// <summary> It allows alter dynamically behaviors. </summary>
+    public abstract class Decorable
+    {
+        /// <summary> Change the behavior. </summary>
+        /// <value>Decorator component</value>
+        public IDecorator Decorator { get; set; }
+
+        /// <summary> Perform the operation if there is an associated behavior. </summary>
+        public void Operation() => Decorator?.Operation();
+    }
+
+    /// <summary> It allows alter dynamically behaviors. </summary>
+    public abstract class Decorable<TR>
+    {
+        /// <summary> Change the behavior. </summary>
+        /// <value>Decorator component</value>
+        public IDecorator<TR> Decorator { get; set; }
+
+        /// <summary> Perform the operation if there is an associated behavior. </summary>
+        /// <value>Value</value>
+        public TR Operation() => Decorator != null ? Decorator.Operation() : default;
+    }
+
+    /// <summary> It allows alter dynamically behaviors. </summary>
+    public abstract class Decorable<TR, T>
+    {
+        /// <summary> Change the behavior. </summary>
+        /// <value>Decorator component</value>
+        public IDecorator<TR, T> Decorator { get; set; }
+
+        /// <summary> Perform the operation if there is an associated behavior. </summary>
+        /// <param name="value">Value</param>
+        /// <returns>Value</returns>
+        public TR Operation(T value) => Decorator != null ? Decorator.Operation(value) : default;
+    }
+
+    /// <summary> It allows alter dynamically behaviors. </summary>
+    public abstract class Decorable<TR, T0, T1>
+    {
+        /// <summary> Change the behavior. </summary>
+        /// <value>Decorator component</value>
+        public IDecorator<TR, T0, T1> Decorator { get; set; }
+
+        /// <summary> Perform the operation if there is an associated behavior. </summary>
+        /// <param name="value0">First value</param>
+        /// <param name="value1">Second value</param>
+        /// <returns>Value</returns>
+        public TR Operation(T0 value0, T1 value1) => Decorator != null ? Decorator.Operation(value0, value1) : default;
+    }
+}
---
+++ Assets/Heart/Core/Runtime/Patterns/Structural/Decorator/IDecorator.cs
@@ -0,0 +1,30 @@
+﻿namespace Pancake.Pattern
+{
+    /// <summary> Operation that can be altered by decorators. </summary>
+    public interface IDecorator
+    {
+        /// <summary> The operation to be executed. </summary>
+        void Operation();
+    }
+
+    /// <summary> Operation that can be altered by decorators. </summary>
+    public interface IDecorator<out TR>
+    {
+        /// <summary> The operation to be executed. </summary>
+        TR Operation();
+    }
+
+    /// <summary> Operation that can be altered by decorators. </summary>
+    public interface IDecorator<out TR, in T>
+    {
+        /// <summary> The operation to be executed. </summary>
+        TR Operation(T value);
+    }
+
+    /// <summary> Operation that can be altered by decorators. </summary>
+    public interface IDecorator<out TR, in T0, in T1>
+    {
+        /// <summary> The operation to be executed. </summary>
+        TR Operation(T0 value0, T1 value1);
+    }
+}
