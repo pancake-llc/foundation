4a928c7ad0675fb4c036d898b569a70a37372938
aprius <phongsoyenmoc.diep@gmail.com>
2024-11-21 23:12:00 +0700

    -add

---
+++ Assets/Heart/Core/Runtime/MVP/IModel.cs
@@ -0,0 +1,29 @@
+﻿using Pancake.Common;
+
+namespace Pancake
+{
+    public interface IModel<TData>
+    {
+        void Initialize();
+        TData GetData();
+        void SetData(TData data);
+    }
+
+    public abstract class BaseModel<TData> : IModel<TData>
+    {
+        protected TData data;
+        public event System.Action OnDataChanged;
+
+        public virtual void Initialize() { }
+
+        public virtual TData GetData() => data;
+
+        public virtual void SetData(TData newData)
+        {
+            data = newData;
+            NotifyDataChanged();
+        }
+
+        protected void NotifyDataChanged() { OnDataChanged?.Invoke(); }
+    }
+}
