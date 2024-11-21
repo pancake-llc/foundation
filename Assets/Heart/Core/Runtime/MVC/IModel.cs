efabbc51b1caf04b2b47ce34e5a6a48e0b0a3ba5
aprius <phongsoyenmoc.diep@gmail.com>
2024-11-21 18:14:33 +0700

    -wip

---
+++ Assets/Heart/Core/Runtime/MVC/IModel.cs
@@ -0,0 +1,17 @@
+﻿using Pancake.Common;
+
+namespace Pancake
+{
+    public interface IModel<T>
+    {
+        ObservableList<T> Datas { get; }
+        void Add(T data);
+    }
+
+    public abstract class BaseModel<T> : IModel<T>
+    {
+        public ObservableList<T> Datas { get; } = new();
+
+        public void Add(T data) { Datas.Add(data); }
+    }
+}
