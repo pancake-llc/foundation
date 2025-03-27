c99a8fff2a466f0dd8f19c6288398f28ba17e4f7
aprius <phongsoyenmoc.diep@gmail.com>
2025-03-27 23:08:54 +0700

    -add: more extension for nav

--- Assets/Heart/Modules/AI/Runtime/NavMeshExtension.cs
+++ Assets/Heart/Modules/AI/Runtime/NavMeshExtension.cs
@@ -1,4 +1,7 @@
 #if PANCAKE_AI
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Pancake.Common;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -8,7 +11,7 @@ namespace Pancake.AI
     {
         public static bool RandomPositionNavInsideUnitSphere(Vector3 center, float range, out Vector3 result, int numberQuery = 3, float maxDistance = 1f)
         {
-            for (int i = 0; i < numberQuery; i++)
+            for (var i = 0; i < numberQuery; i++)
             {
                 var randomPoint = center + Random.insideUnitSphere * range;
                 if (NavMesh.SamplePosition(randomPoint, out var hit, maxDistance, NavMesh.AllAreas))
@@ -24,7 +27,7 @@ namespace Pancake.AI
 
         public static bool RandomPositionNavOnUnitSphere(Vector3 center, float range, out Vector3 result, int numberQuery = 3, float maxDistance = 1f)
         {
-            for (int i = 0; i < numberQuery; i++)
+            for (var i = 0; i < numberQuery; i++)
             {
                 var randomPoint = center + Random.onUnitSphere * range;
                 if (NavMesh.SamplePosition(randomPoint, out var hit, maxDistance, NavMesh.AllAreas))
@@ -53,6 +56,107 @@ namespace Pancake.AI
 
             nearestNavMeshPoint = checkPosition;
             return false;
+        }
+
+        public static NavMeshAgent IncreaseAngularSpeed(this NavMeshAgent agent, float angularSpeed = 1000)
+        {
+            agent.angularSpeed = angularSpeed;
+            return agent;
+        }
+
+        public static bool HasReachedDestination(this NavMeshAgent agent)
+        {
+            if (agent == null) return false;
+            return !agent.pathPending && agent.remainingDistance <= agent.stoppingDistance;
+        }
+
+        public static bool HasReachedDestination(this NavMeshAgent agent, Transform destination, float tolerence = 0.1f) =>
+            agent.transform.Distance(destination) < tolerence;
+
+        public static bool HasReachedDestination(this NavMeshAgent agent, Vector3 destination, float tolerence = 0.1f) =>
+            agent.transform.Distance(destination) < tolerence;
+
+        public static bool SetRandomDestination(this NavMeshAgent agent, float radius, Vector3? origin = null, int areaMask = NavMesh.AllAreas, int numberQuery = 3)
+        {
+            if (agent == null) return false;
+
+            for (var i = 0; i < numberQuery; i++)
+            {
+                var randomDirection = Random.insideUnitSphere * radius;
+                randomDirection += origin ?? agent.transform.position;
+                if (NavMesh.SamplePosition(randomDirection, out var hit, radius, areaMask))
+                {
+                    agent.SetDestination(hit.position);
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public static async Task SmoothSpeedChange(this NavMeshAgent agent, float targetSpeed, float duration)
+        {
+            if (agent == null) return;
+            float startSpeed = agent.speed;
+            var elapsedTime = 0f;
+            while (elapsedTime < duration)
+            {
+                if (agent == null) return;
+                agent.speed = Mathf.Lerp(startSpeed, targetSpeed, elapsedTime / duration);
+                elapsedTime += Time.deltaTime;
+                await Awaitable.EndOfFrameAsync();
+            }
+
+            agent.speed = targetSpeed;
+        }
+
+        public static NavMeshAgent PatrolDestination(this NavMeshAgent agent, List<Vector3> patrolPath, float tolerance = 1f)
+        {
+            if (agent == null || patrolPath == null || patrolPath.Count == 0) return null;
+
+            var currentWaypoint = patrolPath[0];
+            if (Vector3.Distance(agent.transform.position, currentWaypoint) <= tolerance)
+            {
+                int nextIndex = (patrolPath.IndexOf(currentWaypoint) + 1) % patrolPath.Count;
+                currentWaypoint = patrolPath[nextIndex];
+            }
+
+            agent.SetDestination(currentWaypoint);
+            return agent;
+        }
+
+        /// <summary>
+        /// Call it from a loop
+        /// </summary>
+        public static NavMeshAgent PatrolDestination(this NavMeshAgent agent, List<Transform> patrolPath, float tolerance = 1f)
+        {
+            if (agent == null || patrolPath == null || patrolPath.Count == 0) return null;
+
+            var currentWaypoint = patrolPath[0];
+            if (Vector3.Distance(agent.transform.position, currentWaypoint.position) <= tolerance)
+            {
+                int nextIndex = (patrolPath.IndexOf(currentWaypoint) + 1) % patrolPath.Count;
+                currentWaypoint = patrolPath[nextIndex];
+            }
+
+            agent.SetDestination(currentWaypoint.position);
+            return agent;
+        }
+
+        public static NavMeshAgent AddKnockBack(this NavMeshAgent agent, Transform target, float force, bool useSetDestination = false)
+        {
+            if (agent == null || target == null) return agent;
+
+            agent.ResetPath();
+            var selfPosition = agent.transform.position;
+            var knockBackDirection = target.position.WithY(0) - selfPosition.WithY(0);
+            if (knockBackDirection.sqrMagnitude > 0.01f)
+            {
+                if (useSetDestination) agent.SetDestination(selfPosition + knockBackDirection.normalized * force);
+                else agent.velocity = knockBackDirection.normalized * force;
+            }
+
+            return agent;
         }
     }
 }
