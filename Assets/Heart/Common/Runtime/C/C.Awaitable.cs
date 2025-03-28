cd7a73fec617841966f4b774fa5daafb1ff74e06
aprius <phongsoyenmoc.diep@gmail.com>
2025-03-28 16:38:47 +0700

    -update: NavMeshExtension

---
+++ Assets/Heart/Common/Runtime/C/C.Awaitable.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading;
+
+
+namespace Pancake.Common
+{
+    public static partial class C
+    {
+        public static async UnityEngine.Awaitable WaitUntil(Func<bool> condition, CancellationToken cancellationToken = default)
+        {
+            while (!condition())
+                await UnityEngine.Awaitable.EndOfFrameAsync(cancellationToken);
+        }
+    }
+}
--- Assets/Heart/Modules/AI/Runtime/NavMeshExtension.cs
+++ Assets/Heart/Modules/AI/Runtime/NavMeshExtension.cs
@@ -1,9 +1,13 @@
 #if PANCAKE_AI
+using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using System.Threading.Tasks;
 using Pancake.Common;
 using UnityEngine;
 using UnityEngine.AI;
+using Random = UnityEngine.Random;
 
 namespace Pancake.AI
 {
@@ -110,9 +114,9 @@ namespace Pancake.AI
             agent.speed = targetSpeed;
         }
 
-        public static NavMeshAgent PatrolDestination(this NavMeshAgent agent, List<Vector3> patrolPath, float tolerance = 1f)
+        public static void PatrolDestination(this NavMeshAgent agent, List<Vector3> patrolPath, float tolerance = 1f)
         {
-            if (agent == null || patrolPath == null || patrolPath.Count == 0) return null;
+            if (agent == null || patrolPath == null || patrolPath.Count == 0) return;
 
             var currentWaypoint = patrolPath[0];
             if (Vector3.Distance(agent.transform.position, currentWaypoint) <= tolerance)
@@ -122,15 +126,14 @@ namespace Pancake.AI
             }
 
             agent.SetDestination(currentWaypoint);
-            return agent;
         }
 
         /// <summary>
         /// Call it from a loop
         /// </summary>
-        public static NavMeshAgent PatrolDestination(this NavMeshAgent agent, List<Transform> patrolPath, float tolerance = 1f)
+        public static void PatrolDestination(this NavMeshAgent agent, List<Transform> patrolPath, float tolerance = 1f)
         {
-            if (agent == null || patrolPath == null || patrolPath.Count == 0) return null;
+            if (agent == null || patrolPath == null || patrolPath.Count == 0) return;
 
             var currentWaypoint = patrolPath[0];
             if (Vector3.Distance(agent.transform.position, currentWaypoint.position) <= tolerance)
@@ -140,7 +143,6 @@ namespace Pancake.AI
             }
 
             agent.SetDestination(currentWaypoint.position);
-            return agent;
         }
 
         public static NavMeshAgent AddKnockBack(this NavMeshAgent agent, Transform target, float force, bool useSetDestination = false)
@@ -157,6 +159,202 @@ namespace Pancake.AI
             }
 
             return agent;
+        }
+
+        public static async void SetTemporarySpeed(this NavMeshAgent agent, float temporarySpeed, float duration, CancellationToken cancellationToken = default)
+        {
+            if (agent == null || !agent.isActiveAndEnabled) return;
+            await TemporarySpeedCoroutine(agent, temporarySpeed, duration);
+            return;
+
+            async Task TemporarySpeedCoroutine(NavMeshAgent agent, float temporarySpeed, float duration)
+            {
+                float originalSpeed = agent.speed;
+                agent.speed = temporarySpeed;
+
+                await Awaitable.WaitForSecondsAsync(duration, cancellationToken);
+
+                if (agent != null && agent.isActiveAndEnabled) agent.speed = originalSpeed;
+            }
+        }
+
+        public static void Wander(
+            this NavMeshAgent agent,
+            Func<float> radius,
+            bool isContinues = true,
+            Func<float> waitTime = null,
+            Func<bool> loopWhile = null,
+            Action onStartMoving = null,
+            Action onStopMoving = null,
+            Action onUpdate = null,
+            CancellationToken cancellationToken = default)
+        {
+            if (agent == null || !agent.isActiveAndEnabled) return;
+
+            if (isContinues) WanderCoroutine();
+            else agent.SetRandomDestination(radius());
+
+
+            async void WanderCoroutine()
+            {
+                while (agent != null && agent.isActiveAndEnabled && (loopWhile?.Invoke() ?? true))
+                {
+                    if (agent.SetRandomDestination(radius()))
+                    {
+                        onStartMoving?.Invoke();
+                        await C.WaitUntil(agent.HasReachedDestination, cancellationToken);
+                        onStopMoving?.Invoke();
+                    }
+
+                    await Awaitable.WaitForSecondsAsync(waitTime?.Invoke() ?? 1, cancellationToken);
+                    onUpdate?.Invoke();
+                }
+            }
+        }
+
+        public static void Chase(
+            this NavMeshAgent agent,
+            Func<Transform> target,
+            Func<float> minDistanceKeep,
+            Func<float> maxDistanceKeep,
+            Func<float> delayBetweenSettingDestination = null,
+            Func<bool> loopWhile = null,
+            Func<float> distanceToPlayer = null,
+            Action onUpdate = null,
+            CancellationToken cancellationToken = default)
+        {
+            if (agent == null || !agent.isActiveAndEnabled || target == null) return;
+            ChaseTargetCoroutine();
+            return;
+
+            async void ChaseTargetCoroutine()
+            {
+                var selfPosition = agent.transform.position;
+
+                while (agent != null && agent.isActiveAndEnabled && (loopWhile?.Invoke() ?? true))
+                {
+                    float distance = distanceToPlayer?.Invoke() ?? agent.transform.Distance(target());
+
+                    if (minDistanceKeep == null || maxDistanceKeep == null) agent.SetDestination(target().position);
+                    else
+                    {
+                        if (distance < minDistanceKeep())
+                        {
+                            var directionToMoveWhenTooCloseToPlayer = (target().position - selfPosition).normalized;
+                            var positionToMove = selfPosition + (directionToMoveWhenTooCloseToPlayer * -1 * (maxDistanceKeep() - distance));
+                            var path = new NavMeshPath();
+                            if (agent.CalculatePath(positionToMove, path))
+                                agent.SetDestination(positionToMove);
+                        }
+                        else agent.SetDestination(target().position);
+                    }
+
+                    if (delayBetweenSettingDestination == null) await Awaitable.EndOfFrameAsync(cancellationToken);
+                    else await Awaitable.WaitForSecondsAsync(delayBetweenSettingDestination.Invoke(), cancellationToken);
+
+                    onUpdate?.Invoke();
+                }
+            }
+        }
+
+        public static void Flee(
+            this NavMeshAgent agent,
+            Func<Transform> target = null,
+            Func<float> fleeDistance = null,
+            Func<bool> loopWhile = null,
+            Action onUpdate = null,
+            CancellationToken cancellationToken = default)
+        {
+            if (agent == null || !agent.isActiveAndEnabled || target == null) return;
+            FleeFromTargetCoroutine();
+
+            return;
+
+            async void FleeFromTargetCoroutine()
+            {
+                while (agent != null && agent.isActiveAndEnabled && (loopWhile?.Invoke() ?? true))
+                {
+                    float fleeDistanceValue = fleeDistance?.Invoke() ?? 10;
+                    var fleeDirection = (agent.transform.position - target().position).normalized;
+                    var fleePosition = agent.transform.position + fleeDirection * fleeDistanceValue;
+
+                    if (NavMesh.SamplePosition(fleePosition, out var hit, fleeDistanceValue, NavMesh.AllAreas))
+                    {
+                        agent.SetDestination(hit.position);
+                    }
+
+                    await Awaitable.EndOfFrameAsync(cancellationToken);
+                    onUpdate?.Invoke();
+                }
+            }
+        }
+
+        public static void Patroll(
+            this NavMeshAgent agent,
+            Func<List<Transform>> waypoints,
+            float tolerance = 0.1f,
+            Func<float> waitTime = null,
+            Func<bool> followWaypointOrder = null,
+            Func<bool> loopWhile = null,
+            Action onStartMoving = null,
+            Action onStopMoving = null,
+            Action onUpdate = null,
+            CancellationToken cancellationToken = default)
+        {
+            PatrolWaypointsCoroutine();
+            return;
+
+            async void PatrolWaypointsCoroutine()
+            {
+                var currentWaypointIndex = 0;
+
+                while (agent != null && agent.isActiveAndEnabled && (loopWhile?.Invoke() ?? true))
+                {
+                    var currentWaypoint = waypoints()[currentWaypointIndex];
+                    agent.SetDestination(currentWaypoint.position);
+                    onStartMoving?.Invoke();
+                    await C.WaitUntil(() => agent.HasReachedDestination(currentWaypoint.position, tolerance), cancellationToken);
+                    onStopMoving?.Invoke();
+                    await Awaitable.WaitForSecondsAsync(waitTime?.Invoke() ?? 1, cancellationToken);
+                    currentWaypointIndex = followWaypointOrder?.Invoke() ?? true ? (currentWaypointIndex + 1) % waypoints().Count : Random.Range(0, waypoints().Count);
+                    onUpdate?.Invoke();
+                }
+            }
+        }
+
+        public static void ContinuesAvoidObstaclesWhile(
+            this NavMeshAgent agent,
+            LayerMask obstacleMask,
+            float avoidanceRadius,
+            Func<bool> condition = null,
+            CancellationToken cancellationToken = default)
+        {
+            if (agent == null || !agent.isActiveAndEnabled) return;
+            AvoidObstaclesCoroutine();
+            return;
+
+            async void AvoidObstaclesCoroutine()
+            {
+                while (agent != null && agent.isActiveAndEnabled && (condition?.Invoke() ?? true))
+                {
+                    var obstacles = PhysicsCast.OverlapSphereNonAlloc(agent.transform.position, avoidanceRadius, obstacleMask);
+                    if (obstacles.Length > 0)
+                    {
+                        var avoidanceDirection = Vector3.zero;
+                        foreach (var obstacle in obstacles)
+                        {
+                            avoidanceDirection += (agent.transform.position - obstacle.transform.position).normalized;
+                        }
+
+                        avoidanceDirection /= obstacles.Length;
+
+                        var newDestination = agent.transform.position + avoidanceDirection * avoidanceRadius;
+                        if (NavMesh.SamplePosition(newDestination, out var hit, avoidanceRadius, NavMesh.AllAreas)) agent.SetDestination(hit.position);
+                    }
+
+                    await Awaitable.EndOfFrameAsync(cancellationToken);
+                }
+            }
         }
     }
 }
